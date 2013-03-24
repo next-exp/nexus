6763bd513aca88e4780dbb331e5306dacb4a9e3e
jmalbos <jmalbos@users.noreply.github.com>
2013-03-24 12:22:54 +0000

    Updates to IonizationHit.

--- source/sensdet/IonizationHit.cc
+++ source/sensdet/IonizationHit.cc
@@ -1,82 +1,56 @@
 // ----------------------------------------------------------------------------
 //  $Id$
 //
-//  Author:  J. Martin-Albo <jmalbos@ific.uv.es>
+//  Author:  <justo.martin-albo@ific.uv.es>
 //  Created: 27 Apr 2009
 //
-//  Copyright (c) 2009 NEXT Collaboration 
+//  Copyother (c) 2009-2013 NEXT Collaboration. All others reserved.
 // ----------------------------------------------------------------------------
 
 #include "IonizationHit.h"
 
+using namespace nexus;
 
 
 
-namespace nexus {
-
-
-  G4Allocator<IonizationHit> IonizationHitAllocator;
+G4Allocator<IonizationHit> IonizationHitAllocator;
   
 
   
-  IonizationHit::IonizationHit()
-  {
-  }
+IonizationHit::IonizationHit(): G4VHit()
+{
+}
 
   
   
-  IonizationHit::IonizationHit(const IonizationHit& right): G4VHit()
-  {
-    *this = right;
-  }
+IonizationHit::IonizationHit(const IonizationHit& other): G4VHit()
+{
+  *this = other;
+}
   
 
 
-  IonizationHit::~IonizationHit()
-  {
-  }
+IonizationHit::~IonizationHit()
+{
+}
   
   
   
-  const IonizationHit& IonizationHit::operator =(const IonizationHit& right)
-  {
-    _track_id   = right._track_id;
-    _time       = right._time;
-    _energy_dep = right._energy_dep;
-    _position   = right._position;
+const IonizationHit& IonizationHit::operator=(const IonizationHit& other)
+{
+  _track_id   = other._track_id;
+  _time       = other._time;
+  _energy_dep = other._energy_dep;
+  _position   = other._position;
     
-    return *this;
-  }
+  return *this;
+}
   
   
   
-  G4int IonizationHit::operator ==(const IonizationHit& right) const
-  {
-    return (this==&right) ? 1 : 0;
-  }
+G4int IonizationHit::operator==(const IonizationHit& other) const
+{
+  return (this==&other) ? 1 : 0;
+}
   
   
-  
-  // bhep::hit* IonizationHit::ToBhep(const G4String& det_name) const
-  // {
-  //   // create a bhep hit
-  //   bhep::hit* bhit = new bhep::hit(bhep::TRUTH, det_name);
-
-  //   // set properties
-  //   bhit->set_point(bhep::Point3D(_position.x(),
-		// 		  _position.y(),
-		// 		  _position.z()));
-
-  //   bhit->set_amplitude(_energy_dep);
-  //   bhit->set_time(_time);
-
-  //   // associate hit to particle
-  //   bhep::particle& bpart = BhepUtils::GetBParticle(_track_id);
-  //   bhit->set_mother_particle(bpart);
-  //   bpart.add_hit(det_name, bhit);
-    
-  //   return bhit;
-  // }
-  
-
-} // end namespace nexus
--- source/sensdet/IonizationHit.h
+++ source/sensdet/IonizationHit.h
@@ -2,11 +2,11 @@
 ///  \file   IonizationHit.h
 ///  \brief  Ionization deposit left by a particle in an active volume.
 ///
-///  \author   J Martin-Albo <jmalbos@ific.uv.es>
+///  \author   <justo.martin-albo@ific.uv.es>
 ///  \date     27 Apr 2009
 ///  \version  $Id$ 
 ///
-///  Copyright (c) 2009 NEXT Collaboration
+///  Copyright (c) 2009-2013 NEXT Collaboration. All rights reserved.
 // ----------------------------------------------------------------------------
 
 #ifndef __IONIZATION_HIT__
@@ -26,6 +26,23 @@ namespace nexus {
   class IonizationHit: public G4VHit
   {
   public:
+    /// Constructor
+    IonizationHit();
+    /// Copy constructor
+    IonizationHit(const IonizationHit&);
+    /// Destructor
+    virtual ~IonizationHit();
+    
+    /// Assignement operator
+    const IonizationHit& operator=(const IonizationHit&);
+    /// Equality operator
+    G4int operator==(const IonizationHit&) const;
+    /// Memory allocation
+    void* operator new(size_t);
+    /// Memory deallocation
+    void operator delete(void*);
+
+  public:
     G4int GetTrackID();
     void SetTrackID(G4int);
     
@@ -38,25 +55,6 @@ namespace nexus {
     G4ThreeVector GetPosition();
     void SetPosition(G4ThreeVector);
 
-//    bhep::hit* ToBhep(const G4String& det_name) const;
-
-  public:
-    /// Constructor
-    IonizationHit();
-    /// Destructor
-    ~IonizationHit();
-    /// Copy-constructor
-    IonizationHit(const IonizationHit&);
-    /// Assignement operator
-    const IonizationHit& operator =(const IonizationHit&);
-    /// Equality operator
-    G4int operator ==(const IonizationHit&) const;
-    
-    /// memory allocation
-    void* operator new(size_t);
-    /// memory deallocation
-    inline void operator delete(void*);
-    
   private:
     G4int _track_id;         
     G4double _time;
@@ -66,23 +64,16 @@ namespace nexus {
   
   
   typedef G4THitsCollection<IonizationHit> IonizationHitsCollection;
-  
   extern G4Allocator<IonizationHit> IonizationHitAllocator;
 
   
-  // inline methods ........................................
-  
-  // memory management
+  // INLINE DEFINITIONS //////////////////////////////////////////////
   
   inline void* IonizationHit::operator new(size_t) 
-  { void* aHit;
-    aHit = (void*) IonizationHitAllocator.MallocSingle();
-    return aHit; }
+  { return ((void*) IonizationHitAllocator.MallocSingle()); }
   
   inline void IonizationHit::operator delete(void* aHit)
   { IonizationHitAllocator.FreeSingle((IonizationHit*) aHit); }
-
-  // setters & getters
 
   inline G4int IonizationHit::GetTrackID() { return _track_id; }
   inline void IonizationHit::SetTrackID(G4int id) { _track_id = id; }
@@ -95,7 +86,8 @@ namespace nexus {
   { _energy_dep = edep; }
 
   inline G4ThreeVector IonizationHit::GetPosition() { return _position; }
-  inline void IonizationHit::SetPosition(G4ThreeVector xyz) { _position = xyz; }
+  inline void IonizationHit::SetPosition(G4ThreeVector xyz) 
+  { _position = xyz; }
 
 
 } // end namespace nexus
