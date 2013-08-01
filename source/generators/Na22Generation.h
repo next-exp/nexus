003c6580a08da0783a4581c552b55749d3579771
paola <paola@bd53911d-e828-0410-8381-a983918e8cd0>
2013-08-01 09:05:10 +0000

    Na22 generator modified with messengers. It compiles, yet not tested to run

--- source/generators/Na22Generation.h
+++ source/generators/Na22Generation.h
@@ -5,6 +5,7 @@
 
 class G4Event;
 class G4ParticleDefinition;
+class G4GenericMessenger;
 
 namespace nexus {
 
@@ -29,10 +30,11 @@ namespace nexus {
     //Destructor
     ~Na22Generation();
 
-    void ReadConfigParams();
     void GeneratePrimaryVertex(G4Event* evt);
 
   private:
+
+    G4GenericMessenger* _msg;
     
     G4double _energy_min; ///< Minimum possible kinetic energy for annihilation particle
     G4double _energy_max; ///< Maximum possible kinetic energy for annihilation particle
