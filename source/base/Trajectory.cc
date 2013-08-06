98f2d2dab5fd4878eb2aab9cace3748057830cef
jmalbos <jmalbos@users.noreply.github.com>
2013-08-06 20:06:41 +0000

    Updates on class Trajectory.

--- source/base/Trajectory.cc
+++ source/base/Trajectory.cc
@@ -14,8 +14,8 @@
 #include <G4Track.hh>
 #include <G4ParticleDefinition.hh>
 
-
 using namespace nexus;
+
 
 
 G4Allocator<Trajectory> TrjAllocator;
@@ -67,16 +67,9 @@ G4ParticleDefinition* Trajectory::GetParticleDefinition()
 
 
 
-G4int Trajectory::GetTrackID() const
+void Trajectory::SetParticleDefinition(G4ParticleDefinition* pdef)
 {
-  return _trackId;
-}
-
-
-
-G4int Trajectory::GetParentID() const
-{
-  return _parentId;
+  _pdef = pdef;
 }
 
 
@@ -88,13 +81,6 @@ G4String Trajectory::GetParticleName() const
 
 
 
-G4double Trajectory::GetCharge() const
-{
-  return _pdef->GetPDGCharge();
-}
-
-
-
 G4int Trajectory::GetPDGEncoding() const
 {
   return _pdef->GetPDGEncoding();
@@ -102,22 +88,19 @@ G4int Trajectory::GetPDGEncoding() const
 
 
 
-G4ThreeVector Trajectory::GetInitialMomentum() const
+G4double Trajectory::GetCharge() const
 {
-  return _initial_momentum;
+  return _pdef->GetPDGCharge();
 }
-
-
-
 
 
 
 void Trajectory::AppendStep(const G4Step* aStep)
 {
-  if (_record_trjpoints) {
-    TrajectoryPoint* point = new TrajectoryPoint();
-    _trjpoints->push_back(point);
-  }
+  if (!_record_trjpoints) return;
+
+  TrajectoryPoint* point = new TrajectoryPoint();
+  _trjpoints->push_back(point);
 }
 
 
@@ -137,100 +120,5 @@ void Trajectory::MergeTrajectory(G4VTrajectory* second)
   delete (*tmp->_trjpoints)[0];
   tmp->_trjpoints->clear();
 }
-
-
-
-// void Trajectory::ShowTrajectory(std::ostream& os) const
-// {
-//   // Invoke the default implementation in the base class
-//   G4VTrajectory::ShowTrajectory(os);
-// }
-
-
-
-// void Trajectory::DrawTrajectory(G4int i_mode) const
-// {
-//   // Invoke the default implementation in the base class
-//   G4VTrajectory::DrawTrajectory(i_mode);
-// }
-
-
-
-// const std::map<G4String,G4AttDef>* Trajectory::GetAttDefs() const
-// {
-//   G4bool isNew;
-//   std::map<G4String,G4AttDef>* store
-//     = G4AttDefStore::GetInstance("Trajectory",isNew);
-  
-//   if (isNew) {
-//     G4String ID("ID");
-//     (*store)[ID] = G4AttDef(ID,"Track ID","Physics","","G4int");
-
-//     G4String PID("PID");
-//     (*store)[PID] = G4AttDef(PID,"Parent ID","Physics","","G4int");
-
-//     G4String PN("PN");
-//     (*store)[PN] = G4AttDef(PN,"Particle Name","Physics","","G4String");
-
-//     G4String Ch("Ch");
-//     (*store)[Ch] = G4AttDef(Ch,"Charge","Physics","e+","G4double");
-
-//     G4String PDG("PDG");
-//     (*store)[PDG] = G4AttDef(PDG,"PDG Encoding","Physics","","G4int");
-
-//     G4String IKE("IKE");
-//     (*store)[IKE] = G4AttDef(IKE, "Initial kinetic energy",
-//       "Physics","G4BestUnit","G4double");
-
-//     G4String IMom("IMom");
-//     (*store)[IMom] = G4AttDef(IMom, "Initial momentum",
-//       "Physics","G4BestUnit","G4ThreeVector");
-
-//     G4String IMag("IMag");
-//     (*store)[IMag] = G4AttDef(IMag, "Initial momentum magnitude",
-//       "Physics","G4BestUnit","G4double");
-
-//     G4String NTP("NTP");
-//     (*store)[NTP] = G4AttDef(NTP,"No. of points","Physics","","G4int");
-//   }
-
-//   return store;
-// }
-
-
-
-// std::vector<G4AttValue>* Trajectory::CreateAttValues() const
-// {
-//   std::vector<G4AttValue>* values = new std::vector<G4AttValue>;
- 
-//   values->push_back
-//     (G4AttValue("ID",G4UIcommand::ConvertToString(fTrackID),""));
- 
-//   values->push_back
-//     (G4AttValue("PID",G4UIcommand::ConvertToString(fParentID),""));
- 
-//   values->push_back
-//     (G4AttValue("PN",ParticleName,""));
- 
-//   values->push_back
-//     (G4AttValue("Ch",G4UIcommand::ConvertToString(PDGCharge),""));
- 
-//   values->push_back
-//     (G4AttValue("PDG",G4UIcommand::ConvertToString(PDGEncoding),""));
- 
-//   values->push_back
-//     (G4AttValue("IKE",G4BestUnit(initialKineticEnergy,"Energy"),""));
- 
-//   values->push_back
-//     (G4AttValue("IMom",G4BestUnit(initialMomentum,"Energy"),""));
- 
-//   values->push_back
-//     (G4AttValue("IMag",G4BestUnit(initialMomentum.mag(),"Energy"),""));
- 
-//   values->push_back
-//     (G4AttValue("NTP",G4UIcommand::ConvertToString(GetPointEntries()),""));
- 
-//   return values;
-// }
 
  
--- source/base/Trajectory.h
+++ source/base/Trajectory.h
@@ -48,15 +48,34 @@ namespace nexus {
 
   public:
 
-    // Getters -- Particle properties
     G4ParticleDefinition* GetParticleDefinition();
-    G4int GetTrackID() const;
-    G4int GetParentID() const;
+    void SetParticleDefinition(G4ParticleDefinition*);
+
     G4String GetParticleName() const;
     G4double GetCharge() const;
-    G4int GetPDGEncoding() const;
+    G4int GetPDGEncoding () const;
+  
+    G4int GetTrackID() const;
+    void SetTrackID(G4int);
+
+    G4int GetParentID() const;
+    void SetParentID(G4int);
+
     G4ThreeVector GetInitialMomentum() const;
+    void SetInitialMomentum(const G4ThreeVector&);
     
+    G4ThreeVector GetInitialPosition() const;
+    void SetInitialPosition(const G4ThreeVector&);
+
+    G4ThreeVector GetFinalPosition() const;
+    void SetFinalPosition(const G4ThreeVector&);
+
+    G4double GetInitialTime() const;
+    void SetInitialTime(G4double);
+
+    G4double GetFinalTime() const;
+    void SetFinalTime(G4double);
+
     
     // Trajectory points
 
@@ -64,22 +83,25 @@ namespace nexus {
     virtual int GetPointEntries() const;
     /// Return the i-th point in the trajectory
     virtual G4VTrajectoryPoint* GetPoint(G4int i) const;
+    ///
     virtual void AppendStep(const G4Step*);
+    ///
     virtual void MergeTrajectory(G4VTrajectory*);
 
-    //virtual void ShowTrajectory(std::ostream& os=G4cout) const;
-    //virtual void DrawTrajectory(G4int i_mode=0) const;
-    //virtual const std::map<G4String, G4AttDef>* GetAttDefs() const;
-    //virtual std::vector<G4AttValue>* CreateAttValues() const;
 
   private:
-    G4ParticleDefinition* _pdef;
+    G4ParticleDefinition* _pdef; //< Pointer to the particle definition
 
     G4int _trackId;   ///< Identification number of the track
     G4int _parentId;  ///< Identification number of the parent particle
 
     G4ThreeVector _initial_momentum;
+
     G4ThreeVector _initial_position;
+    G4ThreeVector _final_position;
+
+    G4double _initial_time;
+    G4double _final_time;
 
     G4bool _record_trjpoints;
 
@@ -110,5 +132,46 @@ inline int nexus::Trajectory::GetPointEntries() const
 inline G4VTrajectoryPoint* nexus::Trajectory::GetPoint(G4int i) const
 { return (*_trjpoints)[i]; }
 
+inline G4ThreeVector nexus::Trajectory::GetInitialMomentum() const
+{ return _initial_momentum; }
+
+inline void nexus::Trajectory::SetInitialMomentum(const G4ThreeVector& p)
+{ _initial_momentum = p; }
+
+inline G4int nexus::Trajectory::GetTrackID() const
+{ return _trackId; }
+
+inline void nexus::Trajectory::SetTrackID(G4int id)
+{ _trackId = id; }
+
+inline G4int nexus::Trajectory::GetParentID() const
+{ return _parentId; }
+
+inline void nexus::Trajectory::SetParentID(G4int id)
+{ _parentId = id; }
+
+inline G4ThreeVector nexus::Trajectory::GetInitialPosition() const
+{ return _initial_position; }
+
+inline void nexus::Trajectory::SetInitialPosition(const G4ThreeVector& x)
+{ _initial_position = x; }
+
+inline G4ThreeVector nexus::Trajectory::GetFinalPosition() const
+{ return _final_position; }
+
+inline void nexus::Trajectory::SetFinalPosition(const G4ThreeVector& x)
+{ _final_position = x; }
+
+inline G4double nexus::Trajectory::GetInitialTime() const
+{ return _initial_time; }
+
+inline void nexus::Trajectory::SetInitialTime(G4double t)
+{ _initial_time = t; }
+
+inline G4double nexus::Trajectory::GetFinalTime() const
+{ return _final_time; }
+
+inline void nexus::Trajectory::SetFinalTime(G4double t)
+{ _final_time = t; }
 
 #endif  
