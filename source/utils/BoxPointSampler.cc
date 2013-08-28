e801c5e0f2932c17300fedf24fcbe4a5d94b5c95
jmalbos <jmalbos@users.noreply.github.com>
2013-08-28 13:57:33 +0000

    Revision of BoxPointSampler.

--- source/utils/BoxPointSampler.cc
+++ source/utils/BoxPointSampler.cc
@@ -4,7 +4,7 @@
 //  Author : Javier Muñoz Vidal <jmunoz@ific.uv.es>
 //  Created: 2 November 2009
 //
-//  Copyright (c) 2009, 2010 NEXT Collaboration
+//  Copyright (c) 2009-2013 NEXT Collaboration. All rights reserved.
 // ----------------------------------------------------------------------------
 
 #include "BoxPointSampler.h"
@@ -14,14 +14,14 @@
 
 namespace nexus {
 
-  BoxPointSampler::BoxPointSampler(G4double inner_x, 
-				   G4double inner_y, 
-				   G4double inner_z,
-				   G4double _thickness, 
-				   G4ThreeVector origin,
-				   G4RotationMatrix* rotation):
+  BoxPointSampler::BoxPointSampler(G4double inner_x,
+                                   G4double inner_y, 
+                        				   G4double inner_z,
+                        				   G4double thickness, 
+				                           G4ThreeVector origin,
+				                           G4RotationMatrix* rotation):
     _inner_x(inner_x), _inner_y(inner_y), _inner_z(inner_z),
-    _thickness(_thickness), _origin(origin), _rotation(rotation)
+    _thickness(thickness), _origin(origin), _rotation(rotation)
   {
     _outer_x = _inner_x + 2.*_thickness;
     _outer_y = _inner_y + 2.*_thickness;
@@ -61,19 +61,25 @@ namespace nexus {
   {
     G4double x, y, z, origin;
     
+    ///////////////////////////////////////////////////////
+
     // Default vertex
     if (region == "CENTER") {
       return G4ThreeVector(0., 0., 0.); 
     }
-    
+
+    ///////////////////////////////////////////////////////
+   
     // Generating in the endcap volume
     else if (region == "Z_VOL") {
       x = GetLength(0., _outer_x);
       y = GetLength(0., _outer_y);
       origin = -0.5 * (_inner_z + _thickness);
       z = GetLength(origin, _thickness);
-      return _rot_trans(G4ThreeVector(x, y, z));
+      return RotateAndTranslate(G4ThreeVector(x, y, z));
     }
+
+    ///////////////////////////////////////////////////////
 
     // Generating in the whole volume
     else if (region == "WHOLE_VOL") {
@@ -82,52 +88,57 @@ namespace nexus {
 
       // Z walls volume
       if (rand < _perc_Zvol) {
-	x = GetLength(0., _outer_x);
-	y = GetLength(0., _outer_y);
-	// Selecting between -Z and +Z
-	if (rand2 < 0.5) origin = -0.5 * (_inner_z + _thickness);
-	else  origin = 0.5 * (_inner_z + _thickness);
-	z = GetLength(origin, _thickness);
+        x = GetLength(0., _outer_x);
+        y = GetLength(0., _outer_y);
+        // Selecting between -Z and +Z
+        if (rand2 < 0.5) origin = -0.5 * (_inner_z + _thickness);
+        else  origin = 0.5 * (_inner_z + _thickness);
+        z = GetLength(origin, _thickness);
       }
 
       // Y walls volume
-      else if ( (_perc_Zvol < rand) and (rand < (_perc_Zvol+_perc_Yvol))) {
-	x = GetLength(0., _outer_x);
-	// Selecting between -Y and +Y
-	if (rand2 < 0.5) origin = -0.5 * (_inner_y + _thickness);
-	else  origin = 0.5 * (_inner_y + _thickness);
-	y = GetLength(origin, _thickness);
-	z = GetLength(0., _inner_z);
+      else if ( (_perc_Zvol < rand) && (rand < (_perc_Zvol+_perc_Yvol))) {
+        x = GetLength(0., _outer_x);
+        // Selecting between -Y and +Y
+        if (rand2 < 0.5) origin = -0.5 * (_inner_y + _thickness);
+        else  origin = 0.5 * (_inner_y + _thickness);
+        y = GetLength(origin, _thickness);
+        z = GetLength(0., _inner_z);
       }
 
       // X walls volume
       else {
-	// Selecting between -X and +X
-	if (rand2 < 0.5) origin = -0.5 * (_inner_x + _thickness);
-	else  origin = 0.5 * (_inner_x + _thickness);
-	x = GetLength(origin, _thickness);
-	y = GetLength(0., _inner_y);
-	z = GetLength(0., _inner_z);
+        // Selecting between -X and +X
+        if (rand2 < 0.5) origin = -0.5 * (_inner_x + _thickness);
+        else  origin = 0.5 * (_inner_x + _thickness);
+        x = GetLength(origin, _thickness);
+        y = GetLength(0., _inner_y);
+        z = GetLength(0., _inner_z);
       }
-
-      return _rot_trans(G4ThreeVector(x, y, z));
+      return RotateAndTranslate(G4ThreeVector(x, y, z));
     }
+
+    ///////////////////////////////////////////////////////
     
     // Generating in the volume inside
     else if (region == "INSIDE") {
       x = GetLength(0., _inner_x);
       y = GetLength(0., _inner_y);
       z = GetLength(0., _inner_z);
-      return _rot_trans(G4ThreeVector(x, y, z));
+      return RotateAndTranslate(G4ThreeVector(x, y, z));
     }
+
+    ///////////////////////////////////////////////////////
 
     // Generating in the endcap surface
     else if (region == "Z_SURF") {
       x = GetLength(0., _inner_x);
       y = GetLength(0., _inner_y);
       z = -0.5 * _inner_z;
-      return _rot_trans(G4ThreeVector(x, y, z));
+      return RotateAndTranslate(G4ThreeVector(x, y, z));
     }
+
+    ///////////////////////////////////////////////////////
    
     // Generating in the whole surface
     else if (region == "WHOLE_SURF") {
@@ -136,54 +147,59 @@ namespace nexus {
 
       // Z walls surface
       if (rand < _perc_Zsurf) {
-	x = GetLength(0., _inner_x);
-	y = GetLength(0., _inner_y);
-	// +Z wall surface
-	if (rand2 < 0.5) z = 0.5 * _inner_z;
-	// -Z wall surface
-	else  z = -0.5 * _inner_z;
+        x = GetLength(0., _inner_x);
+        y = GetLength(0., _inner_y);
+        // +Z wall surface
+        if (rand2 < 0.5) z = 0.5 * _inner_z;
+        // -Z wall surface
+        else  z = -0.5 * _inner_z;
       }
 
       // Y walls surface
-      else if ((_perc_Zsurf < rand) and (rand < (_perc_Zsurf+_perc_Ysurf))) {
-	x = GetLength(0., _inner_x);
-	// +Y wall surface
-	if (rand2 < 0.5) y = 0.5 * _inner_y;
-	// -Y wall surface
-	else y = -0.5 * _inner_y;
-	z = GetLength(0., _inner_z);
+      else if ((_perc_Zsurf < rand) && (rand < (_perc_Zsurf+_perc_Ysurf))) {
+        x = GetLength(0., _inner_x);
+        // +Y wall surface
+        if (rand2 < 0.5) y = 0.5 * _inner_y;
+        // -Y wall surface
+        else y = -0.5 * _inner_y;
+        z = GetLength(0., _inner_z);
       }
 
       // X walls surface
       else {
-	// +X wall surface
-	if (rand2 < 0.5) x = 0.5 * _inner_x;
-	// -X wall surface
-	else x = -0.5 * _inner_x;
-	y = GetLength(0., _inner_y);
-	z = GetLength(0., _inner_z);
+      // +X wall surface
+        if (rand2 < 0.5) x = 0.5 * _inner_x;
+        // -X wall surface
+        else x = -0.5 * _inner_x;
+        y = GetLength(0., _inner_y);
+        z = GetLength(0., _inner_z);
       }
 
-      return _rot_trans(G4ThreeVector(x, y, z));
+      return RotateAndTranslate(G4ThreeVector(x, y, z));
     }
+
+    ///////////////////////////////////////////////////////
 
     // Unknown region
     else {
-      G4cout << "\n\nERROR. BoxPointSampler::Unknown Region\n\n";
-      exit(0);
+      G4String err = "Unknown generation region: " + region;
+      G4Exception("GenerateVertex()", "[BoxPointSampler]",
+        FatalErrorInArgument, err);
     }
   }
 
 
 
-  G4double BoxPointSampler::GetLength(G4double origin, G4double max_length) {
+  G4double BoxPointSampler::GetLength(G4double origin, G4double max_length) 
+  {
     G4double rand = G4UniformRand() - 0.5;
     return origin + (rand * max_length);
   }
 
 
 
-  G4ThreeVector BoxPointSampler::_rot_trans(G4ThreeVector position) {
+  G4ThreeVector BoxPointSampler::RotateAndTranslate(G4ThreeVector position) 
+  {
     G4ThreeVector real_pos = position;
 
     // Rotating if needed
@@ -193,5 +209,4 @@ namespace nexus {
     return real_pos;
   }
   
-
 } // end namespace nexus
--- source/utils/BoxPointSampler.h
+++ source/utils/BoxPointSampler.h
@@ -1,16 +1,16 @@
 // ----------------------------------------------------------------------------
 ///  \file   BoxPointSampler.h
-///  \brief  Sampler of random uniform points in a box-shape volume.
+///  \brief  Sampler of random uniform points in a box-shaped volume.
 ///
 ///  \author   Javier Muñoz Vidal <jmunoz@ific.uv.es>
 ///  \date     2 November 2009
 ///  \version  $Id$
 ///
-///  Copyright (c) 2009, 2010 NEXT Collaboration
+///  Copyright (c) 2009-2013 NEXT Collaboration. All rights reserved.
 // ----------------------------------------------------------------------------
 
-#ifndef __BOX_POINT_SAMPLER__
-#define __BOX_POINT_SAMPLER__
+#ifndef BOX_POINT_SAMPLER_H
+#define BOX_POINT_SAMPLER_H
 
 #include <G4ThreeVector.hh>
 #include <G4RotationMatrix.hh>
@@ -18,15 +18,16 @@
 
 namespace nexus {
 
+  /// Sampler of random uniform points in a box-shaped volume
 
   class BoxPointSampler
   {
   public:
     /// Constructor
     BoxPointSampler(G4double inner_x, G4double inner_y, G4double inner_z, 
-		       G4double _thickness,
-		       G4ThreeVector origin=G4ThreeVector(0.,0.,0.),
-		       G4RotationMatrix* rotation=0);
+		                G4double _thickness,
+          		      G4ThreeVector origin=G4ThreeVector(0.,0.,0.),
+		                G4RotationMatrix* rotation=0);
     
     /// Destructor
     ~BoxPointSampler();
@@ -36,15 +37,14 @@ namespace nexus {
     
   private:
     G4double GetLength(G4double origin, G4double max_length);
-    G4ThreeVector _rot_trans(G4ThreeVector position);
+    G4ThreeVector RotateAndTranslate(G4ThreeVector position);
 						    
   private:
-    
     G4double _inner_x, _inner_y, _inner_z; ///< Internal dimensions
-    G4double _thickness; ///< Walls thickness
-    
     G4double _outer_x, _outer_y, _outer_z; ///< External dimensions
-    G4double _perc_Zvol, _perc_Yvol; ///< Faces volumes percentages
+    G4double _thickness; ///< Walls thickness
+ 
+    G4double _perc_Zvol, _perc_Yvol;   ///< Faces volumes percentages
     G4double _perc_Zsurf, _perc_Ysurf; ///< Faces surfaces percentages
 
     G4ThreeVector _origin;
