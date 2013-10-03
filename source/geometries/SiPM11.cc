972792b60e11c8d1b4bf65e3253b008bcf97d872
ander <ander@bd53911d-e828-0410-8381-a983918e8cd0>
2013-10-03 09:44:23 +0000

    New SiPM geometry and efficiency added

--- source/geometries/SiPM11.cc
+++ source/geometries/SiPM11.cc
@@ -62,18 +62,34 @@ namespace nexus {
     G4Box* sipm_solid = new G4Box("SIPM11", sipm_x/2., sipm_y/2., sipm_z/2);
 
     G4Material* epoxy = MaterialsList::Epoxy();
-    epoxy->SetMaterialPropertiesTable(OpticalMaterialProperties::Epoxy());
+    epoxy->SetMaterialPropertiesTable(OpticalMaterialProperties::GlassEpoxy());
     
     G4LogicalVolume* sipm_logic = 
       new G4LogicalVolume(sipm_solid, epoxy, "SIPM11");
 
     this->SetLogicalVolume(sipm_logic);
 
+
+    // PCB ///////////////////////////////////////////////////////
+
+    G4double pcb_z = 0.550 * mm;
     
+    G4Material* plastic = G4NistManager::Instance()->FindOrBuildMaterial("G4_POLYCARBONATE");
+
+    G4Box* plastic_solid = new G4Box("PLASTIC", sipm_x/2., sipm_y/2., pcb_z/2);
+    
+    G4LogicalVolume* plastic_logic = 
+    new G4LogicalVolume(plastic_solid, plastic, "PLASTIC");
+    
+    G4double epoxy_z = 0.300 * mm;
+
+    new G4PVPlacement(0, G4ThreeVector(0, 0., epoxy_z/2), plastic_logic,
+		      "PLASTIC", sipm_logic, false, 0, true);
+
     // ACTIVE WINDOW /////////////////////////////////////////////////
-    
+
     G4double active_side     = 1.0   * mm;
-    G4double active_depth    = 0.3   * mm;
+    G4double active_depth    = 0.01   * mm;
     G4double active_offset_x = 0.975 * mm;
     
     G4Box* active_solid =
@@ -86,21 +102,36 @@ namespace nexus {
       new G4LogicalVolume(active_solid, silicon, "PHOTODIODES");
 
     G4double pos_x = - sipm_x/2. + active_offset_x + active_side/2.;
-    G4double pos_z = sipm_z/2. - active_depth/2.;
+    G4double pos_z = epoxy_z/2. - active_depth/2. - pcb_z/2;
 
     new G4PVPlacement(0, G4ThreeVector(pos_x, 0., pos_z), active_logic,
-		      "PHOTODIODES", sipm_logic, false, 0);
+		      "PHOTODIODES", sipm_logic, false, 0, true);
     
     
     // OPTICAL SURFACES //////////////////////////////////////////////
 
-    // TODO. Set proper sensitivity of the SiPM.
+    // SiPM efficiency set using the official Hamamatsu specs.
     
-    const G4int entries = 2;
+    const G4int entries = 26;
     
-    G4double energies[entries]     = {2.0*eV, 10.*eV};
-    G4double reflectivity[entries] = {0.    ,  0.   };
-    G4double efficiency[entries]   = {1.    ,  1.   };
+    G4double energies[entries]     = {1.405*eV, 1.456*eV, 1.515*eV, 1.597*eV, 1.689*eV,
+				      1.763*eV, 1.836*eV, 1.915*eV, 2.007*eV, 2.105*eV, 
+				      2.190*eV, 2.285*eV, 2.366*eV, 2.448*eV, 2.563*eV, 
+				      2.718*eV, 2.838*eV, 2.977*eV, 3.099*eV, 3.243*eV, 
+				      3.387*eV, 3.525*eV, 3.608*eV, 3.695*eV, 3.762*eV, 
+				      3.857*eV };
+    G4double reflectivity[entries] = {0.      ,0.      ,0.      ,0.      ,0.      ,
+				      0.      ,0.      ,0.      ,0.      ,0.      ,
+				      0.      ,0.      ,0.      ,0.      ,0.      ,
+				      0.      ,0.      ,0.      ,0.      ,0.      ,
+				      0.      ,0.      ,0.      ,0.      ,0.      ,
+				      0.      };
+    G4double efficiency[entries]   = {0.0556  ,0.0698  ,0.0893  ,0.1250  ,0.1661  ,
+				      0.1983  ,0.2341  ,0.2663  ,0.3058  ,0.3488  ,
+				      0.3868  ,0.4247  ,0.4499  ,0.4734  ,0.4915  ,
+				      0.4991  ,0.4898  ,0.4662  ,0.4355  ,0.4002  ,
+				      0.3471  ,0.2878  ,0.2308  ,0.1620  ,0.0804  ,
+				      0.0390  };
     
     G4MaterialPropertiesTable* sipm_mt = new G4MaterialPropertiesTable();
     sipm_mt->AddProperty("EFFICIENCY", energies, efficiency, entries);
