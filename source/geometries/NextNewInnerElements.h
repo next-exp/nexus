bcaa11f0f940e1f91525b4497273d0b70a0f1307
minegui <minegui@bd53911d-e828-0410-8381-a983918e8cd0>
2013-10-14 16:10:08 +0000

    Inner elements call EP+FC+TP

--- source/geometries/NextNewInnerElements.h
+++ source/geometries/NextNewInnerElements.h
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
-///  \file   
-///  \brief  
-///
+///  \file    NextNewInnerElements 
+///  \brief   This class constructs the inner elements of Next-New:
+///           Energy plane, field cage and tracking plane.
 ///  \author   <miquel.nebot@ific.uv.es>
 ///  \date     18 Sept 2013
 ///  \version  $Id$
@@ -12,23 +12,20 @@
 #ifndef __NEXTNEW_INNER_ELEMENTS__
 #define __NEXTNEW_INNER_ELEMENTS__
 
-#include "NextNewFieldCage.h"
-#include "NextNewEnergyPlane.h"
-#include "NextNewTrackingPlane.h"
-#include "CylinderPointSampler.h"
-
+#include "BaseGeometry.h"
 #include <G4ThreeVector.hh>
 
 class G4LogicalVolume;
 class G4Material;
 class G4GenericMessenger;
+namespace nexus { class NextNewEnergyPlane; } 
+namespace nexus { class NextNewFieldCage; } 
+namespace nexus { class NextNewTrackingPlane; }
 
 namespace nexus {
 
-  /// This is a geometry placer that encloses from the FIELD CAGE to inside
-  /// covering the ACTIVE region, EL gap and its grids, the cathode grid ...
-
-  class NextNewInnerElements{
+  class NextNewInnerElements: public BaseGeometry
+  {
 
   public:
     // Constructor
@@ -40,7 +37,7 @@ namespace nexus {
     void SetLogicalVolume(G4LogicalVolume* mother_logic);
 
     // It Returns the relative position respect to the rest of NEXTNEW geometry
-    G4ThreeVector GetPosition() const;
+    //G4ThreeVector GetPosition() const;
 
     /// Generate a vertex within a given region of the geometry
     G4ThreeVector GenerateVertex(const G4String& region) const;
@@ -55,35 +52,18 @@ namespace nexus {
     G4double _pressure;
     G4double _temperature;
 
-    void BuildELRegion();
-    void BuildCathodeGrid();
-    void BuildActive();    
-
-    G4double _max_step_size;
-    
-    G4double _active_diam;
-    G4double _active_length;
-    G4double _tracking_displ, _energy_displ; // Displacement of tracking / energy zones
-    G4double _el_gap;
-    G4double _grid_thickness;
-    G4double _el_grid_transparency, _cathode_grid_transparency;
-
-    G4double _el_grid_ref_z;//?????????????
-    
     // Detector parts
+    NextNewEnergyPlane*   _energy_plane;    
     NextNewFieldCage*     _field_cage;
-    NextNewEnergyPlane*   _energy_plane;
     NextNewTrackingPlane* _tracking_plane;
 
     // Visibilities
-    G4bool _grids_visibility;
-
-    // Vertex Generators
-    CylinderPointSampler* _active_gen;
+    G4bool _visibility;
 
     // Messenger for the definition of control commands
     G4GenericMessenger* _msg; 
-
+    
+    G4double _max_step_size;
   };
 
 } // end namespace nexus
