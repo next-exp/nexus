bf2f21d06ff0968e47f75b38a9b70d58c2adedee
minegui <minegui@bd53911d-e828-0410-8381-a983918e8cd0>
2013-10-15 14:33:53 +0000

    Inner Copper Shielding GEO+VERTEX CHECK

---
+++ source/geometries/NextNewIcs.h
@@ -0,0 +1,73 @@
+// ----------------------------------------------------------------------------
+///  \file   
+///  \brief  
+///
+///  \author   <miquel.nebot@ific.uv.es>,
+///  \date     14 Oct 2013
+///  \version  $Id$
+///
+///  Copyright (c) 2013 NEXT Collaboration
+// ----------------------------------------------------------------------------
+
+#ifndef __NEXTNEW_ICS__
+#define __NEXTNEW_ICS__
+
+#include "BaseGeometry.h"
+#include "CylinderPointSampler.h"
+
+#include <G4Navigator.hh>
+
+class G4GenericMessenger;
+
+namespace nexus {
+
+  class NextNewIcs: public BaseGeometry
+  {
+  public:
+    /// Constructor
+    NextNewIcs()/*const G4double lat_nozzle_in_diam,
+	       const G4double up_nozzle_in_diam,
+	       const G4double lat_anode_nozzle_xpos,
+	       const G4double lat_cathode_nozzle_xpos,
+	       const G4double anode_nozzle_zpos,
+	       const G4double central_nozzle_zpos,
+	       const G4double cathode_nozzle_zpos)*/;
+
+    /// Destructor
+    ~NextNewIcs();
+
+    /// Generate a vertex within a given region of the geometry
+    G4ThreeVector GenerateVertex(const G4String& region) const;
+
+    /// Builder
+    void Construct();
+
+
+  private:
+    // Dimensions
+    G4double _body_out_diam, _body_length, _body_thickness;
+    G4double _tracking_tread_diam, _tracking_tread_length;
+   
+    // Dimensions coming from outside  
+    G4double  _lat_nozzle_in_diam, _lat_nozzle_x_pos, _lat_nozzle_z_pos;
+    G4double  _up_nozzle_in_diam, _up_nozzle_y_pos, _up_nozzle_z_pos;
+
+    // Visibility of the shielding
+    G4bool _visibility;
+
+    // Vertex generators
+    CylinderPointSampler* _body_gen;
+    CylinderPointSampler* _tread_gen;
+    G4double _body_perc;
+
+    // Geometry Navigator
+    G4Navigator* _geom_navigator;
+
+    // Messenger for the definition of control commands
+    G4GenericMessenger* _msg; 
+
+  };
+
+} // end namespace nexus
+
+#endif
