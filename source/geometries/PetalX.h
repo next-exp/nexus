9e914c1095b5dfa2672a68ec33a6effb2c2a1a12
paolafer <paola.ferrario@gmail.com>
2015-05-28 17:33:59 +0200

    first import pf PetalX geometry

---
+++ source/geometries/PetalX.h
@@ -0,0 +1,64 @@
+// ----------------------------------------------------------------------------
+///  \file   PetalX.h
+///  \brief  
+///
+///  \author   <jmunoz@ific.uv.es>
+///  \date     January 2014
+///  \version  $Id$
+///
+///  Copyright (c) 2013 NEXT Collaboration
+// ----------------------------------------------------------------------------
+
+#ifndef PETALX__
+#define PETALX__
+
+#include "BaseGeometry.h"
+#include "BoxPointSampler.h"
+
+class G4LogicalVolume;
+class G4GenericMessenger;
+class G4Material;
+
+
+namespace nexus {
+
+  class PetalX: public BaseGeometry {
+  public:
+    /// Constructor
+    PetalX();
+
+    /// Destructor
+    ~PetalX();
+
+    /// Generate a vertex within a given region of the geometry
+    G4ThreeVector GenerateVertex(const G4String& region) const;
+
+  private:
+    void Construct();
+
+  private:
+
+    void BuildActive(); 
+
+    G4LogicalVolume* lab_logic_;
+
+    // Detector dimensions
+    const G4double detector_size_; /// Size of the Xe box    
+
+    // ACTIVE gas Xenon
+    G4Material* lXe_;
+
+    // Parameters
+    G4double max_step_size_;  /// Maximum Step Size
+    
+    //Vertex generator    
+    BoxPointSampler* active_gen_;
+
+    /// Messenger for the definition of control commands
+    G4GenericMessenger* msg_; 
+
+  };
+  
+} // end namespace nexus
+
+#endif
