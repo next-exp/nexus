d2ee7b8a993c431991c06c814e44afb220a8e393
paullglebrun <lebrun@fnal.gov>
2016-03-28 15:07:43 -0500

    Interface to the C++ version of Decay0

--- source/generators/Decay0Interface.h
+++ source/generators/Decay0Interface.h
@@ -18,7 +18,7 @@
 class G4GenericMessenger;
 class G4Event;
 class G4PrimaryParticle;
-
+class decay0;
 
 namespace nexus {
 
@@ -58,7 +58,20 @@ namespace nexus {
 
     G4bool _opened;
     
+    int _myEventCounter;
+    
+    decay0 *_decay0;
+    int _Xe136DecayMode; // See method printDecayModeList  Default is 1
+    int _Ba136FinalState; // labeled by energy level, in keV . 
+                          // Valid list: 0, 819,  1551, 1579, 2080, 2129, 2141, 2223, 2315, 2400) 
+			  // default is 0 (ground state)  
+    
+    std::ofstream _fOutDebug; // for debugging... 
     const BaseGeometry* _geom;
+    
+    
+    inline void SetXe136DecayMode(int dcm) { _Xe136DecayMode = dcm;}
+    inline void SetBa136FinalState(int fs) { _Ba136FinalState = fs;}
   };
 
   
