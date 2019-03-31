a78af859258e97aea86f9f9743c3b02dd7659e3e
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2019-03-31 11:08:00 -0400

    CMake-based build system completed.

--- cmake/FindGATE.cmake
+++ cmake/FindGATE.cmake
@@ -1,37 +1,40 @@
-## --- FindLog4cpp.cmake ---
-## Try to find Log4cpp library and headers.
+## --- FindGATE.cmake ---
+## Try to find GATE library and headers.
 ## Once this is done, the following variables will be defined:
-##  - Log4cpp_FOUND:        true if Log4cpp was found and false otherwise.
-##  - Log4cpp_INCLUDE_DIRS: location of headers.
-##  - Log4cpp_LIBRARIES:    libraries to link against.
+##  - GATE_FOUND:        true if GATE was found and false otherwise.
+##  - GATE_INCLUDE_DIRS: location of headers.
+##  - GATE_LIBRARIES:    libraries to link against.
 ##
 ## Variables used by this module which can be used to change the default
 ## behaviour, and hence need to be set before calling find_package:
-##  - Log4cpp_ROOT_DIR: installation prefix for searching for Log4cpp.
-##                      Set this if the module has problems finding your
-##                      installation.
-## If Log4cpp is installed in a non-standard way, e.g. a non GNU-style install
+##  - GATE_ROOT_DIR: installation prefix for searching for GATE.
+##                   Set this if the module has problems finding your
+##                   installation.
+## If GATE is installed in a non-standard way, e.g. a non GNU-style install
 ## of <prefix>/{lib,include}, then this module may fail to locate the headers
 ## and libraries as needed. In this case, the following cached variables can
 ## be editted to point to the correct locations.
-##  - Log4cpp_INCLUDE_DIR: path to the Log4cpp include directory (cached)
-##  - Log4cpp_LIBRARY:     path to the Log4cpp library (cached)
+##  - GATE_INCLUDE_DIR: path to the GATE include directory (cached)
+##  - GATE_LIBRARY:     path to the GATE library (cached)
 ## You should not need to set these in the vast majority of cases.
 ##
 
-find_path(Log4cpp_INCLUDE_DIR NAMES log4cpp HINTS ${Log4cpp_ROOT_DIR}/include)
-mark_as_advanced(Log4cpp_INCLUDE_DIR)
+find_path(GATE_INCLUDE_DIR NAMES GATE HINTS ${GATE_ROOT_DIR}/include)
+mark_as_advanced(GATE_INCLUDE_DIR)
 
-find_library(Log4cpp_LIBRARY NAMES log4cpp HINTS ${Log4cpp_ROOT_DIR}/lib)
-mark_as_advanced(Log4cpp_LIBRARY)
+find_library(GATE_LIBRARY NAMES GATE HINTS ${GATE_ROOT_DIR}/lib)
+mark_as_advanced(GATE_LIBRARY)
+
+find_library(GATEIO_LIBRARY NAMES GATEIO HINTS ${GATE_ROOT_DIR}/lib)
+mark_as_advanced(GATEIO_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Log4cpp
-                                  FOUND_VAR     Log4cpp_FOUND
-                                  REQUIRED_VARS Log4cpp_LIBRARY
-                                                Log4cpp_INCLUDE_DIR)
+find_package_handle_standard_args(GATE
+    FOUND_VAR GATE_FOUND
+    REQUIRED_VARS GATE_LIBRARY GATEIO_LIBRARY GATE_INCLUDE_DIR
+    FAIL_MESSAGE "Set GATE_ROOT_DIR to the directory that contains your GATE installation.")
 
-if(Log4cpp_FOUND)
-  set(Log4cpp_LIBRARIES ${Log4cpp_LIBRARY})
-  set(Log4cpp_INCLUDE_DIRS ${Log4cpp_INCLUDE_DIR})
+if(GATE_FOUND)
+  set(GATE_LIBRARIES ${GATE_LIBRARY} ${GATEIO_LIBRARY})
+  set(GATE_INCLUDE_DIRS ${GATE_INCLUDE_DIR})
 endif()
