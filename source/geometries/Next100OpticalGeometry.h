32ea757470a7f87a15496818b4119619d3a3c934
Paola Ferrario <paola.ferrario@gmail.com>
2020-01-01 14:14:38 +0100

    Add new geometry to enclose only NEXT100 inner elements.

---
+++ source/geometries/Next100OpticalGeometry.h
@@ -0,0 +1,60 @@
+// ----------------------------------------------------------------------------
+///  \file   Next100OpticalGeometry.h
+///  \brief  Geometry of the inner elements of NEXT-100.
+///
+///  \author   <paola.ferrario@dipc.org>
+///  \date     30 Dec 2019
+///
+///  Copyright (c) 2019 NEXT Collaboration
+// ----------------------------------------------------------------------------
+
+#ifndef NEXT100_OPT_GEO_H
+#define NEXT100_OPT_GEO_H
+
+#include <G4ThreeVector.hh>
+#include "BaseGeometry.h"
+
+class G4GenericMessenger;
+
+
+namespace nexus {
+
+  class Next100InnerElements;
+
+  /// This is a geometry placer that encloses from the FIELD CAGE to inside
+  /// covering the ACTIVE region, EL gap and its grids, the cathode grid ...
+
+  class Next100OpticalGeometry : public BaseGeometry
+  {
+
+  public:
+    ///Constructor
+    Next100OpticalGeometry();
+
+    /// Destructor
+    ~Next100OpticalGeometry();
+
+    /// Returns a vertex in a region of the geometry
+    G4ThreeVector GenerateVertex(const G4String& region) const;
+
+    /// Builder
+    void Construct();
+
+
+  private:
+
+    // Messenger for the definition of control commands
+    G4GenericMessenger* _msg;
+
+    G4double _pressure;
+    G4double _temperature;
+    G4double _sc_yield;
+    G4String _gas;
+
+    Next100InnerElements* _inner_elements;
+
+  };
+
+} // end namespace nexus
+
+#endif
