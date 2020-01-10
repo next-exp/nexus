aa41a7276d89d7514dcf47fadb84f36626903dbd
Paola Ferrario <paola.ferrario@gmail.com>
2020-01-10 16:08:25 +0100

    Remove translation and rotation from NEXT100.

--- source/geometries/Next100.h
+++ source/geometries/Next100.h
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
-///  \file   
-///  \brief  
+///  \file
+///  \brief
 ///
 ///  \author   <justo.martin-albo@ific.uv.es>, <jmunoz@ific.uv.es>
 ///  \date     21 Nov 2011
@@ -17,8 +17,6 @@
 #include "Next100Vessel.h"
 #include "Next100Ics.h"
 #include "Next100InnerElements.h"
-//#include "Next100FieldCage.h"
-//#include "Next100InternalVolume.h"
 
 class G4LogicalVolume;
 class G4GenericMessenger;
@@ -47,13 +45,13 @@ namespace nexus {
     void BuildLab();
     void Construct();
 
-    
+
   private:
     // Detector dimensions
-    const G4double _lab_size;          /// Size of the air box containing the detector 
+    const G4double _lab_size;          /// Size of the air box containing the detector
 
     // External diameter of nozzles and y positions
-    const G4double _nozzle_ext_diam; 
+    const G4double _nozzle_ext_diam;
     const G4double _up_nozzle_ypos, _central_nozzle_ypos, _down_nozzle_ypos, _bottom_nozzle_ypos;
 
 
@@ -66,20 +64,13 @@ namespace nexus {
     Next100Vessel*    _vessel;
     Next100Ics*       _ics;
     Next100InnerElements* _inner_elements;
-    //Next100FieldCage* _field_cage;
-    /* Next100InternalVolume* _internal_volume; */
 
     BoxPointSampler* _lab_gen; ///< Vertex generator
     MuonsPointSampler* _muon_gen; ///< Vertex generator for muons
 
- 
+
     /// Messenger for the definition of control commands
     G4GenericMessenger* _msg;
-
-    // Rotation around Y and displacement of the whole geometry in the g4 system of reference
-    G4ThreeVector _displ;
-    G4double _rot_angle;
-
   };
 
 } // end namespace nexus
