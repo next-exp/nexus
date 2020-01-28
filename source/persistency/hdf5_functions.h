ae64af87f38fa3beb853562dad1804c47875dc5b
Paola Ferrario <paola.ferrario@gmail.com>
2020-01-28 10:18:06 +0100

    Adapt writer functions to new hdf5 scheme.

--- source/persistency/hdf5_functions.h
+++ source/persistency/hdf5_functions.h
@@ -11,45 +11,54 @@
   typedef struct{
      char param_key[CONFLEN];
      char param_value[CONFLEN];
-   } run_info_t; 
+   } run_info_t;
+
+//  typedef struct{
+//    int32_t evt_number;
+//    float evt_energy;
+//  } evt_t;
 
   typedef struct{
-    int32_t evt_number;
-    uint64_t last_sns_data;
-    uint64_t last_hit;
-    uint64_t last_particle;
-  } evt_extent_t;
-
-  typedef struct{
-    int32_t evt_number;
-    float evt_energy;
-  } evt_t;
-
-  typedef struct{
+    int32_t event_id;
     unsigned int sensor_id;
     uint64_t time_bin;
     unsigned int charge;
   } sns_data_t;
 
   typedef struct{
-	float hit_position[3];
-	float hit_time;
-	float hit_energy;
+        int32_t event_id;
+	float x;
+	float y;
+	float z;
+	float time;
+	float energy;
         char label[STRLEN];
-        int particle_indx;
-        int hit_indx;
+        int particle_id;
+        int hit_id;
   } hit_info_t;
 
   typedef struct{
-	int particle_indx;
+        int32_t event_id;
+	int particle_id;
 	char particle_name[STRLEN];
         char primary;
-	int mother_indx;
-	float initial_vertex[4];
-	float final_vertex[4];
+	int mother_id;
+	float initial_x;
+	float initial_y;
+	float initial_z;
+	float initial_t;
+	float final_x;
+	float final_y;
+	float final_z;
+	float final_t;
         char initial_volume[STRLEN];
         char final_volume[STRLEN];
-	float momentum[3];
+	float initial_momentum_x;
+	float initial_momentum_y;
+	float initial_momentum_z;
+	float final_momentum_x;
+	float final_momentum_y;
+	float final_momentum_z;
 	float kin_energy;
         char creator_proc[PROCLEN];
   } particle_info_t;
@@ -66,18 +75,16 @@
   hsize_t createSensorDataType();
   hsize_t createHitInfoType();
   hsize_t createParticleInfoType();
-  hsize_t createEventExtentType();
   hsize_t createSensorPosType();
 
   hid_t createTable(hid_t group, std::string& table_name, hsize_t memtype);
   hid_t createGroup(hid_t file, std::string& groupName);
 
   void writeRun(run_info_t* runData, hid_t dataset, hid_t memtype, hsize_t counter);
-  void writeEvent(evt_t* evtData, hid_t dataset, hid_t memtype, hsize_t counter);
+//void writeEvent(evt_t* evtData, hid_t dataset, hid_t memtype, hsize_t counter);
   void writeSnsData(sns_data_t* snsData, hid_t dataset, hid_t memtype, hsize_t counter);
   void writeHit(hit_info_t* hitInfo, hid_t dataset, hid_t memtype, hsize_t counter);
   void writeParticle(particle_info_t* particleInfo, hid_t dataset, hid_t memtype, hsize_t counter);
-  void writeEventExtent(evt_extent_t* evtExtent, hid_t dataset, hid_t memtype, hsize_t counter);
   void writeSnsPos(sns_pos_t* snsPos, hid_t dataset, hid_t memtype, hsize_t counter);
 
 
