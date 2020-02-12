09fe5919764b3253684c5f6d4b45fbd3c0f65ada
jmalbos <jmalbos@users.noreply.github.com>
2020-02-12 17:15:32 +0100

    Added new geometry class GenericPhotosensor. It defines a rectangular photosensor of configurable size.

---
+++ source/geometries/GenericPhotosensor.cc
@@ -0,0 +1,123 @@
+// -----------------------------------------------------------------------------
+//  nexus | GenericPhotosensor.cc
+//
+//  * Author: <justo.martin-albo@ific.uv.es>
+//  * Creation date: 22 January 2020
+// -----------------------------------------------------------------------------
+
+#include "GenericPhotosensor.h"
+
+#include "MaterialsList.h"
+#include "PmtSD.h"
+
+#include <G4Box.hh>
+#include <G4LogicalVolume.hh>
+#include <G4PVPlacement.hh>
+#include <G4Material.hh>
+#include <G4NistManager.hh>
+#include <G4OpticalSurface.hh>
+#include <G4LogicalSkinSurface.hh>
+#include <G4SDManager.hh>
+
+using namespace nexus;
+
+
+GenericPhotosensor::GenericPhotosensor(G4double width, G4double height):
+  BaseGeometry(), width_(width), height_(height), thickness_(2.*mm)
+{
+}
+
+
+GenericPhotosensor::GenericPhotosensor(G4double size): GenericPhotosensor(size,size)
+{
+}
+
+
+GenericPhotosensor::~GenericPhotosensor()
+{
+}
+
+
+void GenericPhotosensor::Construct()
+{
+  // PHOTOSENSOR ENCASING ////////////////////////////////////////////
+
+  G4String name = "PHOTOSENSOR";
+
+  G4Box* encasing_solid_vol = new G4Box(name, width_/2., height_/2., thickness_/2.);
+
+  G4LogicalVolume* encasing_logic_vol =
+    new G4LogicalVolume(encasing_solid_vol, MaterialsList::FR4(), name);
+
+  BaseGeometry::SetLogicalVolume(encasing_logic_vol);
+
+  // OPTICAL WINDOW //////////////////////////////////////////////////
+
+  G4double window_thickness = thickness_/4.;
+
+  G4Box* window_solid_vol = new G4Box(name, width_/2., height_/2., window_thickness/2.);
+
+  G4LogicalVolume* window_logic_vol =
+    new G4LogicalVolume(window_solid_vol, MaterialsList::OpticalSilicone(), name);
+
+  G4double zpos = height_/2. - window_thickness/2.;
+
+  new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,zpos), window_logic_vol,
+                    name, encasing_logic_vol, false, 0, true);
+
+  // PHOTOSENSITIVE AREA /////////////////////////////////////////////
+
+  G4double sensarea_thickness = 0.1*mm;
+
+  G4Box* sensarea_solid_vol = new G4Box(name, width_/2., height_/2., sensarea_thickness/2.);
+
+  G4LogicalVolume* sensarea_logic_vol =
+    new G4LogicalVolume(sensarea_solid_vol,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_Si"),
+                        name);
+
+  zpos = height_/2. - window_thickness - sensarea_thickness/2.;
+
+  new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,zpos), sensarea_logic_vol,
+                    name, encasing_logic_vol, false, 0, true);
+
+  // OPTICAL PROPERTIES //////////////////////////////////////////////
+
+  // TODO: Review after the MR with the updated optical properties is approved.
+
+  G4double energy[2]       = {1.0*eV, 4.0*eV};
+  G4double reflectivity[2] = {0.0,    0.0};
+  G4double efficiency[2]   = {1.0,    1.0};
+
+  G4MaterialPropertiesTable* photosensor_mpt = new G4MaterialPropertiesTable();
+  photosensor_mpt->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+  photosensor_mpt->AddProperty("EFFICIENCY",   energy, efficiency,   2);
+
+  G4OpticalSurface* photosensor_opsurf =
+    new G4OpticalSurface("PHOTOSENSOR_OPSURF", unified, polished, dielectric_metal);
+  photosensor_opsurf->SetMaterialPropertiesTable(photosensor_mpt);
+
+  new G4LogicalSkinSurface("PHOTOSENSOR_OPSURF", sensarea_logic_vol, photosensor_opsurf);
+
+  // SENSITIVE DETECTOR //////////////////////////////////////////////
+
+  G4String sdname = "/GENERIC_PHOTOSENSOR";
+  G4SDManager* sdmgr = G4SDManager::GetSDMpointer();
+
+  if (!sdmgr->FindSensitiveDetector(sdname, false)) {
+    PmtSD* sensdet = new PmtSD(sdname);
+    sensdet->SetDetectorVolumeDepth(0);
+    sensdet->SetDetectorNamingOrder(1000.);
+    sensdet->SetTimeBinning(1.*microsecond);
+    sensdet->SetMotherVolumeDepth(1);
+
+    G4SDManager::GetSDMpointer()->AddNewDetector(sensdet);
+    encasing_logic_vol->SetSensitiveDetector(sensdet);
+  }
+}
+
+
+G4ThreeVector GenericPhotosensor::GenerateVertex(const G4String&) const
+{
+  return G4ThreeVector(0., 0., 0.);
+}
---
+++ source/geometries/GenericPhotosensor.h
@@ -0,0 +1,44 @@
+// -----------------------------------------------------------------------------
+//  nexus | GenericPhotosensor.h
+//
+//  * Author: <justo.martin-albo@ific.uv.es>
+//  * Creation date: 22 January 2020
+// -----------------------------------------------------------------------------
+
+#ifndef GENERIC_PHOTOSENSOR_H
+#define GENERIC_PHOTOSENSOR_H
+
+#include "BaseGeometry.h"
+
+
+namespace nexus {
+
+  class GenericPhotosensor: public nexus::BaseGeometry
+  {
+  public:
+    // Constructor for a rectangular sensor
+    GenericPhotosensor(G4double width, G4double height);
+    // Constructor for a square sensor
+    GenericPhotosensor(G4double size);
+    // Destructor
+    virtual ~GenericPhotosensor();
+    //
+    virtual void Construct();
+    //
+    virtual G4ThreeVector GenerateVertex(const G4String&) const;
+
+    G4double GetWidth()     const;
+    G4double GetHeight()    const;
+    G4double GetThickness() const;
+
+  private:
+    G4double width_, height_, thickness_;
+  };
+
+  inline G4double GenericPhotosensor::GetWidth() const { return width_; }
+  inline G4double GenericPhotosensor::GetHeight() const { return height_; }
+  inline G4double GenericPhotosensor::GetThickness() const { return thickness_; }
+
+} // namespace nexus
+
+#endif
