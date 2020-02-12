643e4d9f9a1c351745306fc9dfbdbb1f24e1b1e6
jmalbos <jmalbos@users.noreply.github.com>
2020-02-12 19:13:22 +0100

    Rewritten version of the geometry class Next100TrackingPlane.

--- source/geometries/Next100TrackingPlane.cc
+++ source/geometries/Next100TrackingPlane.cc
@@ -1,279 +1,116 @@
-// ----------------------------------------------------------------------------
-//  $Id$
+// -----------------------------------------------------------------------------
+//  nexus | Next100TrackingPlane.cc
 //
-//  Authors: <jmunoz@ific.uv.es>
-//  Created: 25 Apr 2012
-//  
-//  Copyright (c) 2012 NEXT Collaboration
-// ---------------------------------------------------------------------------- 
+//  * Author: <justo.martin-albo@ific.uv.es>
+//  * Creation date: 22 January 2020
+// -----------------------------------------------------------------------------
 
 #include "Next100TrackingPlane.h"
-#include "MaterialsList.h"
-#include "Visibilities.h"
-#include <G4GenericMessenger.hh>
-#include "OpticalMaterialProperties.h"
 
-#include <G4PVPlacement.hh>
-#include <G4VisAttributes.hh>
-#include <G4Material.hh>
-#include <G4LogicalVolume.hh>
-#include <G4Box.hh>
+#include "Next100SiPMBoard.h"
+
 #include <G4Tubs.hh>
-#include <G4SubtractionSolid.hh>
-#include <G4OpticalSurface.hh>
-#include <G4LogicalSkinSurface.hh>
+#include <G4LogicalVolume.hh>
+#include <G4PVPlacement.hh>
 #include <G4NistManager.hh>
-#include <Randomize.hh>
 
-#include <CLHEP/Units/SystemOfUnits.h>
-#include <CLHEP/Units/PhysicalConstants.h>
-#include <stdexcept>
-
-namespace nexus {
-
-  using namespace CLHEP;
-
-  Next100TrackingPlane::Next100TrackingPlane(): 
-    // Dimensions
-    _support_diam (106.0 * cm),
-    _support_thickness (2.0 * cm),
-    _el_gap_z_edge (66.895 * cm),   // This must be consistent withh Next100InnerElements:
-    // el_gap_posz + el_gap_thickness/2. + grid_thickness/2.
-    _z_displ (0.5 * cm),
-    _hole_size (45 * mm),
-
-    // SiPMTs per Dice Board
-    _SiPM_rows (8),
-    _SiPM_columns (8),
-
-    // Number of Dice Boards, DB columns
-    _DB_columns (11),
-    _num_DBs (107),
-    _visibility(0)
-  {
-
-    /// Initializing the geometry navigator (used in vertex generation)
-    _geom_navigator = G4TransportationManager::GetTransportationManager()->GetNavigatorForTracking();
-
-    /// Messenger
-    _msg = new G4GenericMessenger(this, "/Geometry/Next100/", "Control commands of geometry Next100.");
-    _msg->DeclareProperty("tracking_plane_vis", _visibility, "Tracking Plane Visibility");
-
-    // The Dice Board
-    // _dice_board = new NextElDB(_SiPM_rows, _SiPM_columns);
-    _dice_board = new NextNewKDB(_SiPM_rows, _SiPM_columns);
-  }
+using namespace nexus;
 
 
-
-  void Next100TrackingPlane::Construct()
-  {
-
-    // Dice Boards positions
-    _dice_board->Construct();
-    GenerateDBPositions();
-
-    ///// Support plate
-    G4Tubs* support_plate_nh_solid = new G4Tubs("SUPPORT_PLATE_NH", 0., _support_diam/2., _support_thickness/2., 0., twopi);
-
-    // Making DB holes
-    G4Box* support_plate_db_hole_solid = new G4Box("SUPPORT_PLATE_DB_HOLE", _hole_size/2., _hole_size/2., _support_thickness/2. + 2.*mm);
-    G4SubtractionSolid* support_plate_solid = new G4SubtractionSolid("SUPPORT_PLATE", support_plate_nh_solid,
-								     support_plate_db_hole_solid, 0, _DB_positions[0]);
-    for (int i=1; i<_num_DBs; i++) {
-      support_plate_solid = new G4SubtractionSolid("SUPPORT_PLATE", support_plate_solid,
-						   support_plate_db_hole_solid, 0, _DB_positions[i]);
-    }
-
-    G4LogicalVolume* support_plate_logic = new G4LogicalVolume(support_plate_solid,
-							        G4NistManager::Instance()->FindOrBuildMaterial("G4_Cu"),
-							       "SUPPORT_PLATE");
-
-
-    ///// Dice Boards
- 
-    G4LogicalVolume* dice_board_logic = _dice_board->GetLogicalVolume();
-    G4ThreeVector db_dimensions = _dice_board->GetDimensions();
-    G4double db_thickness = db_dimensions.z();
-
-
-    ///// Support Plate placement
-    G4double support_plate_posz = _el_gap_z_edge + _z_displ + db_thickness + _support_thickness/2.;
-    new G4PVPlacement(0, G4ThreeVector(0.,0.,support_plate_posz), support_plate_logic,  "SUPPORT_PLATE", _mother_logic, false, 0);
-
-    ///// Dice Boards placement
-    G4double dice_board_posz = _el_gap_z_edge + _z_displ + db_thickness/2.;
-
-     const std::vector<std::pair<int, G4ThreeVector> > SiPM_positions = 
-      _dice_board->GetPositions();
-    
-    G4ThreeVector pos;
-    for (int i=0; i<_num_DBs; i++) {
-      pos = _DB_positions[i];
-      pos.setZ(dice_board_posz);
-      new G4PVPlacement(0, pos, dice_board_logic,
-			"DICE_BOARD", _mother_logic, false, i+1, false);
-      // Store the absolute positions of SiPMs in gas, for possible checks
-      for (unsigned int si=0; si< SiPM_positions.size(); si++) {
-	G4ThreeVector mypos =  SiPM_positions[si].second;
-	std::pair<int, G4ThreeVector> abs_pos;
-	abs_pos.first = (i+1)*1000+ SiPM_positions[si].first ;
-	abs_pos.second = 
-	  G4ThreeVector(pos.getX()+mypos.getX(), pos.getY()+mypos.getY(), pos.getZ()+mypos.getZ());
-
-	_absSiPMpos.push_back(abs_pos);
-      }
-    }
-
-    // PrintAbsoluteSiPMPos();
-
-    ////////////////////////////////////////////////
-    ////// It remains the electronics to add  //////
-
-
-    // SETTING VISIBILITIES   //////////
-
-    // Dice boards always visible in dark green
-    G4VisAttributes dark_green_col = nexus::DarkGreen();
-    dice_board_logic->SetVisAttributes(dark_green_col);
-    
-
-    if (_visibility) {
-      G4VisAttributes light_brown_col = nexus::CopperBrown();
-      support_plate_logic->SetVisAttributes(light_brown_col);
-      light_brown_col.SetForceSolid(true);
-      support_plate_logic->SetVisAttributes(light_brown_col);
-    }
-    else {
-      support_plate_logic->SetVisAttributes(G4VisAttributes::Invisible);
-    }
-
-
-
-    // VERTEX GENERATORS   //////////
-    _support_gen  = new CylinderPointSampler(_support_diam/2., _support_thickness, 0.,
-					     0., G4ThreeVector(0., 0., support_plate_posz));
-
-    // Vertexes are generated in a thin surface in the backside of the board
-    G4double z_dim = db_dimensions.z();
-    _dice_board_gen = new BoxPointSampler(db_dimensions.x(), db_dimensions.y(), z_dim * .1,
-					  0., G4ThreeVector(0., 0., dice_board_posz + z_dim*.4));
-  }
-
-
-
-  void Next100TrackingPlane::SetLogicalVolume(G4LogicalVolume* mother_logic)
-  {
-    _mother_logic = mother_logic;
-  }
-
-
-
-  Next100TrackingPlane::~Next100TrackingPlane()
-  {
-    delete _support_gen;
-    delete _dice_board_gen;
-  }
-
-
-
-  G4ThreeVector Next100TrackingPlane::GenerateVertex(const G4String& region) const
-  {
-    G4ThreeVector vertex(0., 0., 0.);
-
-    // Support Plate
-    if (region == "TRK_SUPPORT") {
-      G4VPhysicalVolume *VertexVolume;
-      do {
-	vertex = _support_gen->GenerateVertex("INSIDE");
-	// To check its volume, one needs to rotate and shift the vertex
-	// because the check is done using global coordinates
-	G4ThreeVector glob_vtx(vertex);
-	// First rotate, then shift
-	glob_vtx.rotate(pi, G4ThreeVector(0., 1., 0.));
-	glob_vtx = glob_vtx + G4ThreeVector(0, 0, GetELzCoord());
-	VertexVolume = _geom_navigator->LocateGlobalPointAndSetup(glob_vtx, 0, false);
-	//std::cout << VertexVolume->GetName() << std::endl;
-      } while (VertexVolume->GetName() != "SUPPORT_PLATE");
-    }
-
-    // Dice Boards
-    else if (region == "DICE_BOARD") {
-      vertex = _dice_board_gen->GenerateVertex("INSIDE");
-      G4double rand = _num_DBs * G4UniformRand();
-      G4ThreeVector db_pos = _DB_positions[int(rand)];
-      vertex += db_pos;
-    }
-    else {
-      G4Exception("[Next100TrackingPlane]", "GenerateVertex()", FatalException,
-		  "Unknown vertex generation region!");     
-    }
-
-    return vertex;
-
-  }
-
-
-
-  void Next100TrackingPlane::GenerateDBPositions()
-  {
-    /// Function that computes and stores the XY positions of Dice Boards
-
-    G4int num_rows[] = {6, 9, 10, 11, 12, 11, 12, 11, 10, 9, 6};
-   
-    G4int total_positions = 0;
-
-    // Separation between consecutive columns / rows
-    // G4double x_step = (45. + 35.191) * mm;
-    // G4double y_step = (45. + 36.718) * mm;
- 
-    G4double x_step = _dice_board->GetDimensions().getX() + 1.*mm;
-    G4double y_step = _dice_board->GetDimensions().getY() + 1.*mm;
-
-    G4double x_dim = x_step * (_DB_columns -1);
-
-    G4ThreeVector position(0.,0.,0.);
-
-    // For every column
-    for (G4int col=0; col<_DB_columns; col++) {
-      G4double pos_x = x_dim/2. - col * x_step;
-      G4int rows = num_rows[col];
-      G4double y_dim = y_step * (rows-1);
-      // For every row
-      for (G4int row=0; row<rows; row++) {
-	G4double pos_y = y_dim/2. - row * y_step;
-
-	position.setX(pos_x);
-	position.setY(pos_y);
-	_DB_positions.push_back(position);
-	total_positions++; 
-      }
-    }
-
-    // Checking
-    if (total_positions != _num_DBs) {
-      G4cout << "\n\nERROR: Number of DBs doesn't match with number of positions calculated\n";
-      exit(0);
-    }
-
-
-  }
-
-   void Next100TrackingPlane::PrintAbsoluteSiPMPos()
-  {
-    G4cout << "----- Absolute position of SiPMs in gas volume -----" << G4endl;
-    G4cout <<  G4endl;
-    for (unsigned int i=0; i<_absSiPMpos.size(); i++) {
-      std::pair<int, G4ThreeVector> abs_pos = _absSiPMpos[i];
-      G4cout << "ID number: " << _absSiPMpos[i].first << ", position: " 
-      	     << _absSiPMpos[i].second.getX() << ", "
-      	     << _absSiPMpos[i].second.getY() << ", "
-      	     << _absSiPMpos[i].second.getZ()  << G4endl;
-    }
-    G4cout <<  G4endl;
-    G4cout << "---------------------------------------------------" << G4endl;
-  }
-
+Next100TrackingPlane::Next100TrackingPlane(G4LogicalVolume* mother_volume):
+  BaseGeometry(),
+  copper_plate_diameter_       (1340.*mm),
+  copper_plate_thickness_      ( 120.*mm), 
+  distance_gate_tracking_plane_(  25.*mm),
+  distance_board_board_        (   1.*mm),
+  mother_volume_(mother_volume)
+{
+  // Make sure that the pointer to the mother volume is actually defined
+  if (!mother_volume)
+    G4Exception("[Next100TrackingPlane]", "Next100TrackingPlane()",
+                FatalException, "Mother volume is a nullptr.");
 }
 
 
+Next100TrackingPlane::~Next100TrackingPlane()
+{
+}
+
+
+void Next100TrackingPlane::Construct()
+{
+  // SIPM BOARDS /////////////////////////////////////////////////////
+
+  Next100SiPMBoard sipm_board_geom;
+  sipm_board_geom.Construct();
+  G4LogicalVolume* sipm_board_logic_vol = sipm_board_geom.GetLogicalVolume();
+
+  G4double zpos = - distance_gate_tracking_plane_ - sipm_board_geom.GetThickness()/2.;
+
+  // Central block of 4x8 boards.
+
+  for (auto i=0; i<4; i++) {
+    G4double xpos = - 1.5*sipm_board_geom.GetSize() - 1.5*distance_board_board_
+                    + i*(sipm_board_geom.GetSize() + distance_board_board_);
+
+    for (auto j=0; j<8; j++) {
+      G4double ypos = - 3.5*sipm_board_geom.GetSize() - 3.5*distance_board_board_
+                      + j*(sipm_board_geom.GetSize() + distance_board_board_);
+
+      new G4PVPlacement(nullptr, G4ThreeVector(xpos,ypos,zpos), sipm_board_logic_vol,
+                        sipm_board_logic_vol->GetName(), mother_volume_, false, i+j, true);
+    }
+  }
+
+  // Outer columns of 7 boards
+
+  for (auto j=0; j<7; j++) {
+    G4double xpos = - 2.5*sipm_board_geom.GetSize() - 2.5*distance_board_board_;
+    G4double ypos = - 3.0*sipm_board_geom.GetSize() - 3.0*distance_board_board_
+                    + j*(sipm_board_geom.GetSize() + distance_board_board_);
+
+    new G4PVPlacement(nullptr, G4ThreeVector(xpos,ypos,zpos), sipm_board_logic_vol,
+                      sipm_board_logic_vol->GetName(), mother_volume_, false, j+32, true);
+    new G4PVPlacement(nullptr, G4ThreeVector(-xpos,ypos,zpos), sipm_board_logic_vol,
+                      sipm_board_logic_vol->GetName(), mother_volume_, false, j+39, true);
+  }
+
+  // Outer columns of 5 boards
+
+  for (auto j=0; j<5; j++) {
+    G4double xpos = - 3.5*sipm_board_geom.GetSize() - 3.5*distance_board_board_;
+    G4double ypos = - 2.0*sipm_board_geom.GetSize() - 2.0*distance_board_board_
+                    + j*(sipm_board_geom.GetSize() + distance_board_board_);
+
+    new G4PVPlacement(nullptr, G4ThreeVector(xpos,ypos,zpos), sipm_board_logic_vol,
+                      sipm_board_logic_vol->GetName(), mother_volume_, false, j+46, true);
+    new G4PVPlacement(nullptr, G4ThreeVector(-xpos,ypos,zpos), sipm_board_logic_vol,
+                      sipm_board_logic_vol->GetName(), mother_volume_, false, j+51, true);
+  }
+
+  // TRACKING COPPER PLATE ///////////////////////////////////////////
+
+  G4String copper_plate_name = "TRACKING_COPPER_PLATE";
+
+  G4Tubs* copper_plate_solid_vol = new G4Tubs(copper_plate_name,
+                                             0., copper_plate_diameter_/2.,
+                                             copper_plate_thickness_/2.,
+                                             0., twopi);
+
+  G4LogicalVolume* copper_plate_logic_vol =
+    new G4LogicalVolume(copper_plate_solid_vol,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_Cu"),
+                        copper_plate_name);
+
+  zpos = - distance_gate_tracking_plane_ - sipm_board_geom.GetThickness()
+         - copper_plate_thickness_/2.;
+
+  new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,zpos), copper_plate_logic_vol,
+                   copper_plate_name, mother_volume_, false, 0, true);
+}
+
+
+G4ThreeVector Next100TrackingPlane::GenerateVertex(const G4String&) const
+{
+  return G4ThreeVector();
+}
--- source/geometries/Next100TrackingPlane.h
+++ source/geometries/Next100TrackingPlane.h
@@ -1,96 +1,43 @@
-// ----------------------------------------------------------------------------
-///  \file   
-///  \brief  
-///
-///  \author   <jmunoz@ific.uv.es>
-///  \date     25 Apr 2012
-///  \version  $Id$
-///
-///  Copyright (c) 2012 NEXT Collaboration
-// ----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+//  nexus | Next100TrackingPlane.h
+//
+//  * Author: <justo.martin-albo@ific.uv.es>
+//  * Creation date: 10 February 2020
+// -----------------------------------------------------------------------------
 
-#ifndef __NEXT100_TRACKING_PLANE__
-#define __NEXT100_TRACKING_PLANE__
+#ifndef NEXT100_TRACKING_PLANE_H
+#define NEXT100_TRACKING_PLANE_H
 
-
-#include <G4LogicalVolume.hh>
-#include <G4Navigator.hh>
-#include <G4TransportationManager.hh>
-
-#include "CylinderPointSampler.h"
-#include "BoxPointSampler.h"
-#include "NextElDB.h"
-#include "NextNewKDB.h"
-
-
-class G4Material;
-class G4LogicalVolume;
-class G4GenericMessenger;
+#include "BaseGeometry.h"
 
 
 namespace nexus {
 
-  /// This is a geometry formed by all the components of the tracking plane
-
   class Next100TrackingPlane: public BaseGeometry
   {
-
   public:
-    /// Constructor
+    // Constructor
+    // The mother volume in which all elements of the tracking plane
+    // will be positioned must be provided at construction time.
+    Next100TrackingPlane(G4LogicalVolume* mother_volume);
+    // Destructor
+    virtual ~Next100TrackingPlane();
+    //
+    virtual void Construct();
+    //
+    virtual G4ThreeVector GenerateVertex(const G4String&) const;
+
+  private:
+    // Default constructor hidden
     Next100TrackingPlane();
 
-    /// Destructor
-    ~Next100TrackingPlane();
-
-    // Sets the Logical Volume where Inner Elements will be placed
-    void SetLogicalVolume(G4LogicalVolume* mother_logic);
-
-    /// Generate a vertex within a given region of the geometry
-    G4ThreeVector GenerateVertex(const G4String& region) const;
-
-    /// Builder
-    void Construct();
-
-
   private:
-    void GenerateDBPositions();
-    void PrintAbsoluteSiPMPos();
-
-
-  private:
-
-    // Logical Volume where whole Tracking Plane is placed
-    G4LogicalVolume* _mother_logic;
-
-    // Dice board
-    //   NextElDB* _dice_board;
-    NextNewKDB* _dice_board;
-
-    // Dimensions
-    G4double _support_diam, _support_thickness;
-    G4double _el_gap_z_edge, _z_displ;
-    G4double _hole_size;
-
-    G4int _SiPM_rows, _SiPM_columns;
-    G4int _DB_columns, _num_DBs;
-    std::vector<G4ThreeVector> _DB_positions;
-
-    // Visibility of the tracking plane
-    G4bool _visibility;
-
-    // Vertex generators
-    CylinderPointSampler* _support_gen;
-    BoxPointSampler* _dice_board_gen;
-
-    // Geometry Navigator
-    G4Navigator* _geom_navigator;
-
-    // Messenger for the definition of control commands
-    G4GenericMessenger* _msg; 
-
-    // Container to store the absolute position of SiPMs in gas
-    std::vector<std::pair<int, G4ThreeVector> > _absSiPMpos;
+    G4double copper_plate_diameter_, copper_plate_thickness_;
+    G4double distance_gate_tracking_plane_;
+    G4double distance_board_board_;
+    G4LogicalVolume* mother_volume_;
   };
 
-} //end namespace nexus
+} // namespace nexus
+
 #endif
