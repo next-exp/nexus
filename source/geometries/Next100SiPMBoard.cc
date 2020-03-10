c879e5406dc23ef9024490081e932c3a914ce185
jmalbos <jmalbos@users.noreply.github.com>
2020-03-10 12:01:00 +0100

    Next100SiPMBoard: optical properties revised
    – Teflon has now a skin surface.
    
    – The TPB-gas interface has now border surfaces defined according to
      the model by Benson, Orebi Gann and Gehman.
    
    – Refractive index of generic photosensor matched to TPB.

--- source/geometries/Next100SiPMBoard.cc
+++ source/geometries/Next100SiPMBoard.cc
@@ -10,6 +10,7 @@
 #include "MaterialsList.h"
 #include "GenericPhotosensor.h"
 #include "OpticalMaterialProperties.h"
+#include "BoxPointSampler.h"
 
 #include <G4Box.hh>
 #include <G4LogicalVolume.hh>
@@ -18,6 +19,7 @@
 #include <G4NistManager.hh>
 #include <G4OpticalSurface.hh>
 #include <G4LogicalSkinSurface.hh>
+#include <G4LogicalBorderSurface.hh>
 
 using namespace nexus;
 
@@ -27,35 +29,37 @@ Next100SiPMBoard::Next100SiPMBoard():
   size_     (123.80*mm),
   thickness_(  3.  *mm),
   pitch_    ( 15.60*mm),
-  margin_   (  7.30*mm)
+  margin_   (  7.30*mm),
+  mpv_   (nullptr),
+  vtxgen_(nullptr)
 {
 }
 
 
 Next100SiPMBoard::~Next100SiPMBoard()
 {
+  delete mpv_;
 }
 
 
 void Next100SiPMBoard::Construct()
 {
+  if (!mpv_)
+    G4Exception("[Next100SiPMBoard]", "Construct()",
+                FatalException, "Mother volume is a nullptr.");
+
   // BOARD ///////////////////////////////////////////////////////////
 
-  G4String name = "SIPM_BOARD";
+  G4String board_name = "SIPM_BOARD";
 
-  G4Box* board_solid_vol = new G4Box(name, size_/2., size_/2., thickness_/2.);
+  G4Box* board_solid_vol = new G4Box(board_name, size_/2., size_/2., thickness_/2.);
 
   G4LogicalVolume* board_logic_vol =
     new G4LogicalVolume(board_solid_vol,
                         G4NistManager::Instance()->FindOrBuildMaterial("G4_TEFLON"),
-                        name);
+                        board_name);
 
   BaseGeometry::SetLogicalVolume(board_logic_vol);
-
-  // -------------------------------------------------------
-  // TODO: Review this section once we confirm the
-  //       correctness of the new optical model.
-  // -------------------------------------------------------
 
   G4OpticalSurface* board_opsurf =
     new G4OpticalSurface("SIPM_BOARD_OPSURF", unified, ground, dielectric_metal);
@@ -64,31 +68,38 @@ void Next100SiPMBoard::Construct()
 
   // WLS COATING /////////////////////////////////////////////////////
 
-  name = "SIPM_BOARD_WLS";
-  G4double wls_thickness = 2.*micrometer; // Optimal thickness for TPB
+  G4String wls_name = "SIPM_BOARD_WLS";
+  G4double wls_thickness = 1.*micrometer; // Optimal thickness for TPB
 
-  G4Box* wls_solid_vol = new G4Box(name, size_/2., size_/2., wls_thickness/2.);
+  G4Box* wls_solid_vol = new G4Box(wls_name, size_/2., size_/2., wls_thickness/2.);
 
-  MaterialsList::TPB()->SetMaterialPropertiesTable(OpticalMaterialProperties::Vacuum());
+  G4Material* tpb = MaterialsList::TPB();
+  tpb->SetMaterialPropertiesTable(OpticalMaterialProperties::TPB());
 
   G4LogicalVolume* wls_logic_vol =
-    new G4LogicalVolume(wls_solid_vol, MaterialsList::TPB(), name);
+    new G4LogicalVolume(wls_solid_vol, tpb, wls_name);
 
   G4double zpos = thickness_/2. - wls_thickness/2.;
 
-  new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,zpos), wls_logic_vol,
-                    name, board_logic_vol, false, 0, true);
+  G4VPhysicalVolume* wls_phys_vol =
+    new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,zpos),
+                      wls_logic_vol, wls_name, board_logic_vol,
+                      false, 0, true);
 
-  // -------------------------------------------------------
-  // TODO: Review this section once we confirm the
-  //       correctness of the new optical model.
-  // -------------------------------------------------------
+  G4OpticalSurface* wls_opsurf =
+    new G4OpticalSurface("SIPM_BOARD_WLS_OPSURF",
+                         glisur, ground, dielectric_dielectric, .01);
+  new G4LogicalBorderSurface("SIPM_BOARD_WLS_OPSURF", wls_phys_vol, mpv_, wls_opsurf);
+  new G4LogicalBorderSurface("SIPM_BOARD_WLS_OPSURF", mpv_, wls_phys_vol, wls_opsurf);
 
   // SILICON PHOTOMULTIPLIERS ////////////////////////////////////////
 
   // We use for now the generic photosensor until the exact characteristics
-  // of the new Hamamatsu SiPMs are known
+  // of the new Hamamatsu SiPMs are known.
+  // The refractive index of the sensor's window is matched to that of TPB,
+  // its surrounding medium.
   GenericPhotosensor sipm_geom(1.3*mm);
+  sipm_geom.SetRefractiveIndex(tpb->GetMaterialPropertiesTable()->GetProperty("RINDEX"));
   sipm_geom.Construct();
 
   zpos = thickness_/2. - wls_thickness - 1.*mm;
@@ -101,16 +112,25 @@ void Next100SiPMBoard::Construct()
 
       G4double ypos = -size_/2. + margin_ + j * pitch_;
 
-      new G4PVPlacement(nullptr, G4ThreeVector(xpos, ypos, zpos),
+      G4ThreeVector position(xpos, ypos, zpos);
+      sipm_positions.push_back(position);
+
+      new G4PVPlacement(nullptr, position,
                         sipm_geom.GetLogicalVolume(),
                         sipm_geom.GetLogicalVolume()->GetName(),
-                        board_logic_vol, false, i*10+j, true);
+                        board_logic_vol,
+                        false, i*10+j, true);
     }
   }
+
+  // VERTEX GENERATOR //////////////////////////////////////
+
+  vtxgen_ = new BoxPointSampler(size_, size_, thickness_, 0.);
 }
 
 
 G4ThreeVector Next100SiPMBoard::GenerateVertex(const G4String&) const
 {
-  return G4ThreeVector(0., 0., 0.);
+  // Only one generation region available at the moment
+  return vtxgen_->GenerateVertex("INSIDE");
 }
--- source/geometries/Next100SiPMBoard.h
+++ source/geometries/Next100SiPMBoard.h
@@ -9,9 +9,17 @@
 #define NEXT100_SIPM_BOARD_H
 
 #include "BaseGeometry.h"
+#include <G4ThreeVector.hh>
+#include <vector>
+
+class G4VPhysicalVolume;
 
 
 namespace nexus {
+
+  class BoxPointSampler;
+
+  // Geometry of the 8x8 SiPM boards used in the tracking plane of NEXT-100
 
   class Next100SiPMBoard: public nexus::BaseGeometry
   {
@@ -19,21 +27,30 @@ namespace nexus {
     // Default constructor
     Next100SiPMBoard();
     // Destructor
-    virtual ~Next100SiPMBoard();
+    ~Next100SiPMBoard();
     //
-    virtual void Construct();
+    void SetMotherPhysicalVolume(G4VPhysicalVolume*);
     //
-    virtual G4ThreeVector GenerateVertex(const G4String&) const;
+    void Construct();
+    //
+    G4ThreeVector GenerateVertex(const G4String&) const;
 
     G4double GetSize() const;
     G4double GetThickness() const;
 
   private:
     G4double size_, thickness_, pitch_, margin_;
+    std::vector<G4ThreeVector> sipm_positions;
+    G4VPhysicalVolume* mpv_;
+    BoxPointSampler* vtxgen_;
   };
 
-  inline G4double Next100SiPMBoard::GetSize() const { return size_; }
-  inline G4double Next100SiPMBoard::GetThickness() const { return thickness_; }
+  inline void     Next100SiPMBoard::SetMotherPhysicalVolume(G4VPhysicalVolume* p)
+  { mpv_ = p;}
+  inline G4double Next100SiPMBoard::GetSize() const
+  { return size_; }
+  inline G4double Next100SiPMBoard::GetThickness() const
+  { return thickness_; }
 
 } // namespace nexus
 
