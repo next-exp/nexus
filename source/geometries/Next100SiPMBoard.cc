efa6c1103a67100b717b9f6940540350164d8f72
jmunozv <javier.munoz.vidal@gmail.com>
2020-04-17 17:29:10 +0200

    Next100SiPMBoard - Update to GenericPhotosensor

--- source/geometries/Next100SiPMBoard.cc
+++ source/geometries/Next100SiPMBoard.cc
@@ -30,9 +30,9 @@ Next100SiPMBoard::Next100SiPMBoard():
   thickness_(  3.  *mm),
   pitch_    ( 15.60*mm),
   margin_   (  7.30*mm),
-  mpv_   (nullptr),
-  vtxgen_(nullptr),
-  sipm_geom_(new GenericPhotosensor(1.3*mm))
+  mpv_      (nullptr),
+  vtxgen_   (nullptr),
+  sipm_     (new GenericPhotosensor("SiPM", 1.3 * mm))
 {
 }
 
@@ -40,7 +40,7 @@ Next100SiPMBoard::Next100SiPMBoard():
 Next100SiPMBoard::~Next100SiPMBoard()
 {
   delete mpv_;
-  delete sipm_geom_;
+  delete sipm_;
 }
 
 
@@ -94,16 +94,29 @@ void Next100SiPMBoard::Construct()
   new G4LogicalBorderSurface("SIPM_BOARD_WLS_OPSURF", wls_phys_vol, mpv_, wls_opsurf);
   new G4LogicalBorderSurface("SIPM_BOARD_WLS_OPSURF", mpv_, wls_phys_vol, wls_opsurf);
 
-  // SILICON PHOTOMULTIPLIERS ////////////////////////////////////////
 
+
+  // SILICON PHOTOMULTIPLIERS ////////////////////////////////////////
   // We use for now the generic photosensor until the exact characteristics
   // of the new Hamamatsu SiPMs are known.
-  // The refractive index of the sensor's window is matched to that of TPB,
-  // its surrounding medium.
-  sipm_geom_->Construct();
-  sipm_geom_->SetRefractiveIndex(tpb->GetMaterialPropertiesTable()->GetProperty("RINDEX"));
 
-  zpos = thickness_/2. - wls_thickness - sipm_geom_->GetThickness()/2.;
+  G4MaterialPropertiesTable* photosensor_mpt = new G4MaterialPropertiesTable();
+  G4double energy[]       = {0.2 * eV, 11.5 * eV};
+  G4double reflectivity[] = {0.0     ,  0.0};
+  G4double efficiency[]   = {1.0     ,  1.0};
+  photosensor_mpt->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+  photosensor_mpt->AddProperty("EFFICIENCY",   energy, efficiency,   2);
+  sipm_->SetOpticalProperties(photosensor_mpt);
+
+  sipm_->SetWithWLSCoating(true);
+  //sipm_->SetWindowRefractiveIndex(mpv_->GetLogicalVolume()->GetMaterial()
+  //                                ->GetMaterialPropertiesTable()->GetProperty("RINDEX"));
+
+  sipm_->SetMotherDepth(2);
+  sipm_->Construct();
+
+
+  zpos = thickness_/2. - wls_thickness - sipm_->GetThickness()/2.;
 
   G4int counter = 0;
 
@@ -118,10 +131,8 @@ void Next100SiPMBoard::Construct()
       G4ThreeVector position(xpos, ypos, zpos);
       sipm_positions_.push_back(position);
 
-      new G4PVPlacement(nullptr, position,
-                        sipm_geom_->GetLogicalVolume(),
-                        sipm_geom_->GetLogicalVolume()->GetName(),
-                        board_logic_vol,
+      new G4PVPlacement(nullptr, position, sipm_->GetLogicalVolume(),
+                        sipm_->GetLogicalVolume()->GetName(), board_logic_vol,
                         false, counter, false);
 
       counter++;
