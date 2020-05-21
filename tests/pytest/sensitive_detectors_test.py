52f99a655b7c91f545b91f0975786b8dc693be0e
Paola Ferrario <paola.ferrario@gmail.com>
2020-05-21 18:20:10 +0200

    Modify sensitive detector test accordingly.

--- tests/pytest/sensitive_detectors_test.py
+++ tests/pytest/sensitive_detectors_test.py
@@ -8,22 +8,74 @@ import numpy as np
 
 
 @pytest.fixture(scope="module")
-def input_file(request):
+def nexus_output_file(config_tmpdir, output_tmpdir):
+     base_name = 'NEXT100_sensdet'
+
+     init_text = f"""
+/PhysicsList/RegisterPhysics G4EmStandardPhysics_option4
+/PhysicsList/RegisterPhysics G4DecayPhysics
+/PhysicsList/RegisterPhysics G4RadioactiveDecayPhysics
+/PhysicsList/RegisterPhysics G4OpticalPhysics
+/PhysicsList/RegisterPhysics NexusPhysics
+/PhysicsList/RegisterPhysics G4StepLimiterPhysics
+
+/Geometry/RegisterGeometry NEXT100_OPT
+
+/Generator/RegisterGenerator SINGLE_PARTICLE
+
+/Actions/RegisterTrackingAction DEFAULT
+/Actions/RegisterEventAction DEFAULT
+/Actions/RegisterRunAction DEFAULT
+
+/nexus/RegisterMacro {config_tmpdir}/{base_name}.config.mac
+"""
+     init_path = os.path.join(config_tmpdir, base_name+'.init.mac')
+     init_file = open(init_path,'w')
+     init_file.write(init_text)
+     init_file.close()
+
+     config_text = f"""
+/run/verbose 1
+/event/verbose 0
+/tracking/verbose 0
+
+/Geometry/Next100/elfield true
+/Geometry/Next100/EL_field 10 kV/cm
+/Geometry/Next100/max_step_size 1. mm
+/Geometry/Next100/pressure 15. bar
+/Geometry/Next100/sc_yield 10000 1/MeV
+/Geometry/PmtR11410/SD_depth 3
+
+/Generator/SingleParticle/particle e-
+/Generator/SingleParticle/min_energy 50. keV
+/Generator/SingleParticle/max_energy 50. keV
+/Generator/SingleParticle/region ACTIVE
+
+/nexus/persistency/outputFile {output_tmpdir}/{base_name}
+/nexus/random_seed 21051817
+
+"""
+
+     config_path = os.path.join(config_tmpdir, base_name+'.config.mac')
+     config_file = open(config_path,'w')
+     config_file.write(config_text)
+     config_file.close()
+
      my_env = os.environ.copy()
-     command = ['./nexus', '-b', '-n', '1', 'tests/pytest/test_macros/NEXT100_sensdet.init.mac']
+     command = ['./nexus', '-b', '-n', '1', init_path]
      p = subprocess.run(command, check=True, env=my_env)
-     input_file_for_tests = 'tests/pytest/NEXT100_sensdet.h5'
+     nexus_file_for_tests = os.path.join(output_tmpdir, base_name+'.h5')
 
-     return input_file_for_tests
+     return nexus_file_for_tests
 
 
-def test_sensor_ids_are_sensible(input_file):
+def test_sensor_ids_are_sensible(nexus_output_file):
      """
      Check that we are not registering all photoelectrons
      with the same sensor ID, for each type of sensor.
      """
 
-     sns_response = pd.read_hdf(input_file, 'MC/sns_response')
+     sns_response = pd.read_hdf(nexus_output_file, 'MC/sns_response')
 
      pmts  = sns_response[sns_response.sensor_id < 100]
      sipms = sns_response[sns_response.sensor_id > 100]
