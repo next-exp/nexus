d1300921f6f18353c5d65376d8a894ab5e4afd9f
Paola Ferrario <paola.ferrario@gmail.com>
2020-05-21 18:18:22 +0200

    Add conftest file to define temporary directories.

---
+++ tests/pytest/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture(scope = 'session')
+def config_tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp('configure_tests')
+
+@pytest.fixture(scope = 'session')
+def output_tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp('output_files')
