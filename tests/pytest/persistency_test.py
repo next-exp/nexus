eb257739a89a2a9b5b3d275ba9a995bff0675050
Paola Ferrario <paola.ferrario@gmail.com>
2020-05-22 15:37:23 +0200

    Use more specific name in case other tests use other nexus files.

--- tests/pytest/persistency_test.py
+++ tests/pytest/persistency_test.py
@@ -3,10 +3,10 @@ import tables as tb
 import numpy as np
 
 
-def test_hdf5_structure(nexus_output_file):
+def test_hdf5_structure(nexus_full_output_file):
      """Check that the hdf5 table structure is the correct one."""
 
-     with tb.open_file(nexus_output_file) as h5out:
+     with tb.open_file(nexus_full_output_file) as h5out:
 
          assert 'MC'            in h5out.root
          assert 'particles'     in h5out.root.MC
@@ -76,49 +76,49 @@ def test_hdf5_structure(nexus_output_file):
 
 
 
-def test_particle_ids_of_hits_exist_in_particle_table(nexus_output_file):
+def test_particle_ids_of_hits_exist_in_particle_table(nexus_full_output_file):
     """
     Check that the particle IDs of the hits are also contained
     in the particle table.
     """
 
-    hits = pd.read_hdf(nexus_output_file, 'MC/hits')
+    hits = pd.read_hdf(nexus_full_output_file, 'MC/hits')
     hit_pids = hits.particle_id.unique()
 
-    particles = pd.read_hdf(nexus_output_file, 'MC/particles')
+    particles = pd.read_hdf(nexus_full_output_file, 'MC/particles')
     particle_ids = particles.particle_id.unique()
 
     assert np.all(np.isin(hit_pids, particle_ids))
 
 
-def test_hit_labels(nexus_output_file):
+def test_hit_labels(nexus_full_output_file):
      """Check that there is at least one hit in the ACTIVE volume."""
-     hits = pd.read_hdf(nexus_output_file, 'MC/hits')
+     hits = pd.read_hdf(nexus_full_output_file, 'MC/hits')
      hit_labels = hits.label.unique()
 
      assert 'ACTIVE' in hit_labels
 
 
-def test_primary_always_exists(nexus_output_file):
+def test_primary_always_exists(nexus_full_output_file):
      """Check that there is at least one primary particle."""
-     particles = pd.read_hdf(nexus_output_file, 'MC/particles')
+     particles = pd.read_hdf(nexus_full_output_file, 'MC/particles')
      primary   = particles.primary.unique()
 
      assert 1 in primary
 
 
-def test_sensor_binning_is_saved(nexus_output_file):
+def test_sensor_binning_is_saved(nexus_full_output_file):
      """Check that the sensor binning is saved in the configuration table."""
-     conf = pd.read_hdf(nexus_output_file, 'MC/configuration')
+     conf = pd.read_hdf(nexus_full_output_file, 'MC/configuration')
      parameters = conf.param_key.values
 
      assert any('binning' in p for p in parameters)
 
 
-def test_sensor_names_are_the_same_across_tables(nexus_output_file):
+def test_sensor_names_are_the_same_across_tables(nexus_full_output_file):
      """Check that the sensor labels are the same in all tables."""
-     pos  = pd.read_hdf(nexus_output_file, 'MC/sns_positions')
-     conf = pd.read_hdf(nexus_output_file, 'MC/configuration')
+     pos  = pd.read_hdf(nexus_full_output_file, 'MC/sns_positions')
+     conf = pd.read_hdf(nexus_full_output_file, 'MC/configuration')
 
      pos_labels   = pos.sensor_name.unique()
      parameters   = conf.param_key.values
--- tests/pytest/sensitive_detectors_test.py
+++ tests/pytest/sensitive_detectors_test.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 @pytest.mark.first
-def test_create_nexus_output_file(config_tmpdir, output_tmpdir, base_name, nexus_output_file):
+def test_create_nexus_output_file(config_tmpdir, output_tmpdir, full_base_name, nexus_full_output_file):
 
      init_text = f"""
 /PhysicsList/RegisterPhysics G4EmStandardPhysics_option4
@@ -24,9 +24,9 @@ def test_create_nexus_output_file(config_tmpdir, output_tmpdir, base_name, nexus
 /Actions/RegisterEventAction DEFAULT
 /Actions/RegisterRunAction DEFAULT
 
-/nexus/RegisterMacro {config_tmpdir}/{base_name}.config.mac
+/nexus/RegisterMacro {config_tmpdir}/{full_base_name}.config.mac
 """
-     init_path = os.path.join(config_tmpdir, base_name+'.init.mac')
+     init_path = os.path.join(config_tmpdir, full_base_name+'.init.mac')
      init_file = open(init_path,'w')
      init_file.write(init_text)
      init_file.close()
@@ -48,12 +48,12 @@ def test_create_nexus_output_file(config_tmpdir, output_tmpdir, base_name, nexus
 /Generator/SingleParticle/max_energy 50. keV
 /Generator/SingleParticle/region ACTIVE
 
-/nexus/persistency/outputFile {output_tmpdir}/{base_name}
+/nexus/persistency/outputFile {output_tmpdir}/{full_base_name}
 /nexus/random_seed 21051817
 
 """
 
-     config_path = os.path.join(config_tmpdir, base_name+'.config.mac')
+     config_path = os.path.join(config_tmpdir, full_base_name+'.config.mac')
      config_file = open(config_path,'w')
      config_file.write(config_text)
      config_file.close()
@@ -62,16 +62,16 @@ def test_create_nexus_output_file(config_tmpdir, output_tmpdir, base_name, nexus
      command = ['./nexus', '-b', '-n', '1', init_path]
      p = subprocess.run(command, check=True, env=my_env)
 
-     return nexus_output_file
+     return nexus_full_output_file
 
 
-def test_sensor_ids_are_sensible(nexus_output_file):
+def test_sensor_ids_are_sensible(nexus_full_output_file):
      """
      Check that we are not registering all photoelectrons
      with the same sensor ID, for each type of sensor.
      """
 
-     sns_response = pd.read_hdf(nexus_output_file, 'MC/sns_response')
+     sns_response = pd.read_hdf(nexus_full_output_file, 'MC/sns_response')
 
      pmts  = sns_response[sns_response.sensor_id < 100]
      sipms = sns_response[sns_response.sensor_id > 100]
