9ba3485aade2d0515b6f50d371782b72efd3ff80
Paola Ferrario <paola.ferrario@gmail.com>
2020-05-22 15:37:08 +0200

    Simplify code.

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -12,12 +12,11 @@ def output_tmpdir(tmpdir_factory):
     return tmpdir_factory.mktemp('output_files')
 
 
-@pytest.fixture(scope = 'session',
-                params = ['NEXT100_full_electron.h5'])
-def nexus_output_file(request, output_tmpdir):
-    return os.path.join(output_tmpdir, request.param)
+@pytest.fixture(scope = 'session')
+def full_base_name():
+    return 'NEXT100_full_electron'
 
 
 @pytest.fixture(scope = 'session')
-def base_name():
-    return 'NEXT100_full_electron'
+def nexus_full_output_file(output_tmpdir, full_base_name):
+    return os.path.join(output_tmpdir, full_base_name+'.h5')
