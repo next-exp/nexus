6eee2fb038c62a55cc8a11aa924c851312678db7
Paola Ferrario <paola.ferrario@gmail.com>
2020-05-26 12:06:42 +0200

    Add fixture to parametrize tests over different detectors.
    The idea is to be able to run the same tests over NEW, NEXT100 and
    other future detectors.

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -13,10 +13,28 @@ def output_tmpdir(tmpdir_factory):
 
 
 @pytest.fixture(scope = 'session')
-def full_base_name():
+def full_base_name_new():
+    return 'NEW_full_electron'
+
+
+@pytest.fixture(scope = 'session')
+def nexus_full_output_file_new(output_tmpdir, full_base_name_new):
+    return os.path.join(output_tmpdir, full_base_name_new+'.h5')
+
+
+@pytest.fixture(scope = 'session')
+def full_base_name_next100():
     return 'NEXT100_full_electron'
 
 
 @pytest.fixture(scope = 'session')
-def nexus_full_output_file(output_tmpdir, full_base_name):
-    return os.path.join(output_tmpdir, full_base_name+'.h5')
+def nexus_full_output_file_next100(output_tmpdir, full_base_name_next100):
+    return os.path.join(output_tmpdir, full_base_name_next100+'.h5')
+
+
+@pytest.fixture(scope="module",
+         params=["nexus_full_output_file_new",
+                 "nexus_full_output_file_next100"],
+         ids=["new", "next100"])
+def nexus_files(request):
+    return request.getfixturevalue(request.param)
