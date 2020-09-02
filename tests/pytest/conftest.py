61e0ee86c3b7c5cc71914c07af83605d626e4a2c
jmunozv <javier.munoz.vidal@gmail.com>
2020-09-02 18:28:39 +0200

    Reduce number of fixtures

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -36,33 +36,22 @@ def nexus_full_output_file_next100(output_tmpdir, full_base_name_next100):
     return os.path.join(output_tmpdir, full_base_name_next100 + '.h5')
 
 
-@pytest.fixture(scope = "module",
-                params= ["nexus_full_output_file_new",
-                         "nexus_full_output_file_next100"],
-                ids   = ["new", "next100"])
-def nexus_files(request):
-    return request.getfixturevalue(request.param)
-
-
-
 @pytest.fixture(scope = 'session')
-def new_detector(output_tmpdir, full_base_name_new):
-    det_fname       = os.path.join(output_tmpdir, full_base_name_new + '.h5')
+def new_detector(nexus_full_output_file_new):
     num_pmts        = 12
     num_boards      = 28
     sipms_per_board = 64
     board_ordering  = 1000
-    return det_fname, num_pmts, num_boards, sipms_per_board, board_ordering
+    return nexus_full_output_file_new, num_pmts, num_boards, sipms_per_board, board_ordering
 
 
 @pytest.fixture(scope = 'session')
-def next100_detector(output_tmpdir, full_base_name_next100):
-    det_fname       = os.path.join(output_tmpdir, full_base_name_next100 + '.h5')
+def next100_detector(nexus_full_output_file_next100):
     num_pmts        = 60
     num_boards      = 56
     sipms_per_board = 64
     board_ordering  = 1000
-    return det_fname, num_pmts, num_boards, sipms_per_board, board_ordering
+    return nexus_full_output_file_next100, num_pmts, num_boards, sipms_per_board, board_ordering
 
 
 @pytest.fixture(scope = "module",
