e596a98df42a7c2fb2753e156b4392eed5c43c7f
jmunozv <javier.munoz.vidal@gmail.com>
2020-09-02 11:01:39 +0200

    Parametrize detectors in sesnitive_detectors_test

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -23,7 +23,7 @@ def full_base_name_new():
 
 @pytest.fixture(scope = 'session')
 def nexus_full_output_file_new(output_tmpdir, full_base_name_new):
-    return os.path.join(output_tmpdir, full_base_name_new+'.h5')
+    return os.path.join(output_tmpdir, full_base_name_new + '.h5')
 
 
 @pytest.fixture(scope = 'session')
@@ -33,12 +33,41 @@ def full_base_name_next100():
 
 @pytest.fixture(scope = 'session')
 def nexus_full_output_file_next100(output_tmpdir, full_base_name_next100):
-    return os.path.join(output_tmpdir, full_base_name_next100+'.h5')
+    return os.path.join(output_tmpdir, full_base_name_next100 + '.h5')
 
 
-@pytest.fixture(scope="module",
-         params=["nexus_full_output_file_new",
-                 "nexus_full_output_file_next100"],
-         ids=["new", "next100"])
+@pytest.fixture(scope = "module",
+                params= ["nexus_full_output_file_new",
+                         "nexus_full_output_file_next100"],
+                ids   = ["new", "next100"])
 def nexus_files(request):
+    return request.getfixturevalue(request.param)
+
+
+
+@pytest.fixture(scope = 'session')
+def new_detector(output_tmpdir, full_base_name_new):
+    det_fname       = os.path.join(output_tmpdir, full_base_name_new + '.h5')
+    num_pmts        = 12
+    num_boards      = 28
+    sipms_per_board = 64
+    board_ordering  = 1000
+    return det_fname, num_pmts, num_boards, sipms_per_board, board_ordering
+
+
+@pytest.fixture(scope = 'session')
+def next100_detector(output_tmpdir, full_base_name_next100):
+    det_fname       = os.path.join(output_tmpdir, full_base_name_next100 + '.h5')
+    num_pmts        = 60
+    num_boards      = 56
+    sipms_per_board = 64
+    board_ordering  = 1000
+    return det_fname, num_pmts, num_boards, sipms_per_board, board_ordering
+
+
+@pytest.fixture(scope = "module",
+                params= ["new_detector",
+                         "next100_detector"],
+                ids   = ["new", "next100"])
+def detectors(request):
     return request.getfixturevalue(request.param)
