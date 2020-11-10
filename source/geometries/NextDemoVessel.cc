97108b2c22536dc7c66b7671345f4c4305e05e29
jmalbos <jmalbos@users.noreply.github.com>
2020-11-10 10:14:08 +0100

    Fix relative position of vessel and inner elements

--- source/geometries/NextDemoVessel.cc
+++ source/geometries/NextDemoVessel.cc
@@ -9,9 +9,13 @@
 #include "NextDemoVessel.h"
 
 #include "MaterialsList.h"
+#include "OpticalMaterialProperties.h"
 
+#include <G4GenericMessenger.hh>
 #include <G4Tubs.hh>
 #include <G4LogicalVolume.hh>
+#include <G4PVPlacement.hh>
+#include <G4Material.hh>
 
 using namespace nexus;
 
@@ -19,33 +23,76 @@ using namespace nexus;
 NextDemoVessel::NextDemoVessel():
   BaseGeometry(),
   vessel_diam_  (298.8*mm), // Internal diameter
-  vessel_length_(600.0*mm), // Internal length
-  vessel_thickn_(  3.0*mm)
+  vessel_length_(840.0*mm), // Internal length
+  vessel_thickn_(  3.0*mm),
+  gate_endcap_distance_(-vessel_length_/2. + 92.1*mm),
+  gas_pressure_(15.*bar),
+  gas_temperature_(300.*kelvin),
+  sc_yield_(0.),
+  msg_(nullptr),
+  gas_phys_vol_(nullptr)
 {
+  msg_ = new G4GenericMessenger(this, "/Geometry/NextDemo/",
+                                "Control commands of the NEXT-DEMO geometry.");
+
+  G4GenericMessenger::Command& gas_pressure_cmd =
+    msg_->DeclareProperty("pressure", gas_pressure_, "Xenon gas pressure.");
+  gas_pressure_cmd.SetUnitCategory("Pressure");
+  gas_pressure_cmd.SetParameterName("pressure", false);
+  gas_pressure_cmd.SetRange("pressure>0.");
+
+  G4GenericMessenger::Command& sc_yield_cmd =
+     msg_->DeclareProperty("sc_yield", sc_yield_,
+                           "Scintillation yield (photons/MeV) of xenon gas.");
+  sc_yield_cmd.SetParameterName("sc_yield", true);
+  sc_yield_cmd.SetUnitCategory("1/Energy");
 }
 
 
 NextDemoVessel::~NextDemoVessel()
 {
+  delete msg_;
 }
 
 
 void NextDemoVessel::Construct()
 {
+  ////////////////////////////////////////////////////////////////////
+
   G4String vessel_name = "VESSEL";
 
-  G4Tubs* vessel_solid_vol = new G4Tubs(vessel_name,
-                                        0., (vessel_diam_/2.+vessel_thickn_),
-                                        vessel_length_/2., 0, twopi);
+  G4Tubs* vessel_solid_vol =
+    new G4Tubs(vessel_name, 0., vessel_diam_/2., vessel_length_/2., 0, twopi);
 
   G4LogicalVolume* vessel_logic_vol =
     new G4LogicalVolume(vessel_solid_vol, MaterialsList::Steel(), vessel_name);
 
   BaseGeometry::SetLogicalVolume(vessel_logic_vol);
+
+  ////////////////////////////////////////////////////////////////////
+
+  G4String gas_name = "GAS";
+
+  G4Material* gas_material = MaterialsList::GXe(gas_pressure_, gas_temperature_);
+  gas_material->
+    SetMaterialPropertiesTable(OpticalMaterialProperties::GXe(gas_pressure_,
+                                                              gas_temperature_,
+                                                              sc_yield_));
+
+  G4Tubs* gas_solid_vol = new G4Tubs(gas_name,
+                                     0., (vessel_diam_/2.+vessel_thickn_),
+                                     vessel_length_/2., 0, twopi);
+
+  G4LogicalVolume* gas_logic_vol =
+    new G4LogicalVolume(gas_solid_vol, MaterialsList::Steel(), gas_name);
+
+  gas_phys_vol_ = new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,0.),
+                                    gas_logic_vol, gas_name, vessel_logic_vol,
+                                    false, 0, false);
 }
 
 
-G4ThreeVector NextDemoVessel::GenerateVertex(const G4String& region) const
+G4ThreeVector NextDemoVessel::GenerateVertex(const G4String& /*region*/) const
 {
   return G4ThreeVector();
 }
--- source/geometries/NextDemoVessel.h
+++ source/geometries/NextDemoVessel.h
@@ -11,6 +11,9 @@
 
 #include "BaseGeometry.h"
 
+class G4GenericMessenger;
+class G4VPhysicalVolume;
+
 namespace nexus {
 
   class NextDemoVessel: public BaseGeometry
@@ -20,12 +23,26 @@ namespace nexus {
     ~NextDemoVessel();
     void Construct() override;
     G4ThreeVector GenerateVertex(const G4String& region) const override;
+    G4VPhysicalVolume* GetGasPhysicalVolume();
+    G4double GetGateEndcapDistance() const;
 
   private:
-    const G4double vessel_diam_;
-    const G4double vessel_length_;
-    const G4double vessel_thickn_;
+    const G4double vessel_diam_, vessel_length_, vessel_thickn_;
+    const G4double gate_endcap_distance_;
+
+    G4double gas_pressure_, gas_temperature_;
+    G4double sc_yield_;
+
+    G4GenericMessenger* msg_;
+
+    G4VPhysicalVolume* gas_phys_vol_;
   };
+
+  inline G4VPhysicalVolume* NextDemoVessel::GetGasPhysicalVolume()
+  { return gas_phys_vol_; }
+
+  inline G4double NextDemoVessel::GetGateEndcapDistance() const
+  { return gate_endcap_distance_; }
 
 } // end namespace nexus
 
