e835b5177974e7c82fbb2ba0aa6c0f372a9a17e9
jmalbos <jmalbos@users.noreply.github.com>
2020-11-11 18:47:27 +0100

    Improve file description

--- source/geometries/NextDemoInnerElements.cc
+++ source/geometries/NextDemoInnerElements.cc
@@ -1,7 +1,8 @@
 // ----------------------------------------------------------------------------
 // nexus | NextDemoInnerElements.cc
 //
-// Geometry
+// Wrapper for the placement of the inner elements (field cage and photosensor
+// arrays) of the NEXT-DEMO detector.
 //
 // The NEXT Collaboration
 // ----------------------------------------------------------------------------
