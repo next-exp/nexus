f4d161bd7c5888d6c124d055d51070f898525770
Miryam <mir-1996@hotmail.com>
2020-11-27 18:00:31 +0100

    Remove SingleParticleAngle from GeneratorFactory.cc

--- source/base/GeneratorFactory.cc
+++ source/base/GeneratorFactory.cc
@@ -41,7 +41,6 @@ GeneratorFactory::~GeneratorFactory()
 #include "ELTableGenerator.h"
 #include "ScintillationGenerator.h"
 #include "ElecPositronPairGenerator.h"
-#include "SingleParticleAngle.h"
 
 
 G4VPrimaryGenerator* GeneratorFactory::CreateGenerator() const
@@ -69,8 +68,6 @@ G4VPrimaryGenerator* GeneratorFactory::CreateGenerator() const
   else if (name_ == "SCINTILLATION")   p = new ScintillationGenerator();
 
   else if (name_ == "E+E-PAIR")        p = new ElecPositronPairGenerator();
-
-  else if (name_ == "SINGLE_PARTICLE_ANG") p = new SingleParticleAngle();
 
   else {
     G4String err = "The user specified an unknown generator: " + name_;
