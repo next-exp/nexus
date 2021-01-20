d2ca9c324f9b24750c3ab0dcef4f86d4c1e65e8a
mmkekic <mmkekic@gmail.com>
2021-01-20 20:52:09 +0100

    Divide and rename HistoSampler types
    The class is divided to 3 separete classes, handling 1, 2 and 3d  G4 histograms

--- source/utils/HistoSampler.h
+++ source/utils/HistoSampler.h
@@ -4,27 +4,48 @@
 #include <G4RunManager.hh>
 #include <g4csv.hh>
 
-class PDF{
+class HistoSampler1D{
+  unsigned int nbins    = 0;
+  unsigned int nentries = 0;
+  unsigned int nbins_x  = 0;
+  const tools::histo::h1d * h1G4;
+  std::vector<double> cdf;
+  std::vector<double> edges_x;
+  void create_cdf();
+public:
+  HistoSampler1D(const tools::histo::h1d * hG4):h1G4(hG4){};
+  void sample(double &x, double r);
+};
+
+class HistoSampler2D{
+  unsigned int nbins    = 0;
+  unsigned int nentries = 0;
+  unsigned int nbins_x  = 0;
+  unsigned int nbins_y  = 0;
+  const tools::histo::h2d * h2G4;
+  std::vector<double> cdf;
+  std::vector<double> edges_x;
+  std::vector<double> edges_y;
+  void create_cdf();
+public:
+  HistoSampler2D(const tools::histo::h2d * hG4):h2G4(hG4){};
+  void sample(double &x, double &y, double r1, double r2);
+};
+
+class HistoSampler3D{
   unsigned int nbins    = 0;
   unsigned int nentries = 0;
   unsigned int nbins_x  = 0;
   unsigned int nbins_y  = 0;
   unsigned int nbins_z  = 0;
-  const tools::histo::h1d * h1G4;
-  const tools::histo::h2d * h2G4;
   const tools::histo::h3d * h3G4;
   std::vector<double> cdf;
   std::vector<double> edges_x;
   std::vector<double> edges_y;
   std::vector<double> edges_z;
   void create_cdf();
-  int dim;
 public:
-  PDF(const tools::histo::h1d * hG4):h1G4(hG4), dim(h1G4->dimension()){};
-  PDF(const tools::histo::h2d * hG4):h2G4(hG4), dim(h2G4->dimension()){};
-  PDF(const tools::histo::h3d * hG4):h3G4(hG4), dim(h3G4->dimension()){};
-  void sample(double &x, double r);
-  void sample(double &x, double &y, double r1, double r2);
+  HistoSampler3D(const tools::histo::h3d * hG4):h3G4(hG4){};
   void sample(double &x, double &y, double &z, double r1,double r2, double r3);
 };
 #endif
