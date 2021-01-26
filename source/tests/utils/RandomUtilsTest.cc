bc4d2e65048af3c124aae64a4a0f2a0f70edddfd
Miryam <mir-1996@hotmail.com>
2021-01-26 12:39:14 +0100

    Add 2pi to negative phi.

--- source/tests/utils/RandomUtilsTest.cc
+++ source/tests/utils/RandomUtilsTest.cc
@@ -18,8 +18,6 @@ TEST_CASE("Direction Function") {
     auto costheta_min = 2*(G4UniformRand()-0.5);
     auto phi_max = 2*CLHEP::pi*G4UniformRand();
     auto phi_min = 2*CLHEP::pi*G4UniformRand();
-    //auto phi_max = CLHEP::pi*G4UniformRand();
-    //auto phi_min = CLHEP::pi*G4UniformRand();
 
     if (costheta_max < costheta_min) {
       std::swap(costheta_max,costheta_min);
@@ -28,10 +26,13 @@ TEST_CASE("Direction Function") {
       std::swap(phi_max,phi_min);
     }
 
-    //auto direction = nexus::Direction(costheta_min,costheta_max,phi_min,phi_max);
     G4ThreeVector direction = nexus::Direction(costheta_min,costheta_max,phi_min,phi_max);
     G4double costheta_test = direction.z();
     G4double phi_test = std::atan2(direction.y(), direction.x());
+
+    if (phi_test < 0){
+      phi_test += 2*CLHEP::pi;
+    }
 
     REQUIRE(costheta_max >= costheta_test);
     REQUIRE(costheta_min <= costheta_test);
