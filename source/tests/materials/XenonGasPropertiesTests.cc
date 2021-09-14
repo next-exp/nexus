3efe19215ef6a34a9303bc8146126c793d1436df
Paola Ferrario <paola.ferrario@gmail.com>
2021-09-14 10:44:43 +0200

    Adapt test code to latest changes in xenon properties file.

--- source/tests/materials/XenonGasPropertiesTests.cc
+++ source/tests/materials/XenonGasPropertiesTests.cc
@@ -6,39 +6,37 @@
 
 
 TEST_CASE("XenonProperties::MakeDataTable"){
-  auto props = nexus::XenonProperties();
-  REQUIRE_NOTHROW(props.MakeDataTable());
+  std::vector<std::vector<G4double>> data;
+  REQUIRE_NOTHROW(MakeXeDensityDataTable(data));
 }
 
 TEST_CASE("XenonProperties::Density") {
   // These tests check that the XenonProperties class correctly finds
   // the density of xenon given a temperature and pressure.
 
-  auto props = nexus::XenonProperties();
-
   SECTION ("Density Calculation"){
     Approx target = Approx(87.836).epsilon(0.01);
-    G4double density = props.GetGasDensity(15 * bar, 295 * kelvin);
+    G4double density = GetGasDensity(15 * bar, 295 * kelvin);
     REQUIRE (density/(kg/m3) == target);
   }
 
   SECTION ("Pressure is too big") {
-    REQUIRE_THROWS (props.GetGasDensity(51 * bar, 295 * kelvin),
+    REQUIRE_THROWS (GetGasDensity(51 * bar, 295 * kelvin),
                     "Unknown xenon density for this pressure");
   }
 
   SECTION ("Pressure is too small") {
-    REQUIRE_THROWS (props.GetGasDensity(-15 * bar, 295 * kelvin),
+    REQUIRE_THROWS (GetGasDensity(-15 * bar, 295 * kelvin),
                     "Unknown xenon density for this pressure");
   }
 
   SECTION ("Temperature is too big"){
-    REQUIRE_THROWS (props.GetGasDensity(15 * bar, 410 * kelvin),
+    REQUIRE_THROWS (GetGasDensity(15 * bar, 410 * kelvin),
                     "Unknown xenon density for this temperature");
   }
 
   SECTION ("Temperature is too small"){
-    REQUIRE_THROWS (props.GetGasDensity(15 * bar, -295 * kelvin),
+    REQUIRE_THROWS (GetGasDensity(15 * bar, -295 * kelvin),
                     "Unknown xenon density for this temperature");
   }
 
