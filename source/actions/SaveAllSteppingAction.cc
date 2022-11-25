bf27530bd0f50d70b589ac41e4f7c398cc64910a
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2022-11-25 11:19:27 +0200

    Fix typo

--- source/actions/SaveAllSteppingAction.cc
+++ source/actions/SaveAllSteppingAction.cc
@@ -64,7 +64,7 @@ void SaveAllSteppingAction::UserSteppingAction(const G4Step* step)
   G4ThreeVector initial_pos = pre ->GetPosition();
   G4ThreeVector   final_pos = post->GetPosition();
 
-  if (! post->GetTouchableHandle()->GetVolume()) return; // Particle exists the world
+  if (! post->GetTouchableHandle()->GetVolume()) return; // Particle exits the world
 
   G4String initial_volume = pre ->GetTouchableHandle()->GetVolume()->GetName();
   G4String   final_volume = post->GetTouchableHandle()->GetVolume()->GetName();
