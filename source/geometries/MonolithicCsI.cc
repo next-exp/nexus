b9f1c58a7947a91d3b842ca757c6fbb38005569a
Stefano Roberto Soleti <roberto.soleti@gmail.com>
2023-12-30 13:26:49 +0100

    lut rough esr

--- source/geometries/MonolithicCsI.cc
+++ source/geometries/MonolithicCsI.cc
@@ -120,7 +120,7 @@ namespace nexus
     G4OpticalSurface *ptfe_surface = new G4OpticalSurface("PTFE_SURFACE");
     ptfe_surface->SetType(dielectric_LUT);
     // ptfe_surface->SetFinish(RoughTeflon_LUT);
-    ptfe_surface->SetFinish(groundteflonair);
+    ptfe_surface->SetFinish(groundvm2000air);
     ptfe_surface->SetModel(LUT);
     // ptfe_surface->SetType(dielectric_dielectric);
     // ptfe_surface->SetFinish(polishedvm2000air);
