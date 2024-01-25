b11db1d3b64d71a7a0111f0010f2d0a7ed4816d8
Paola Ferrario <paola.ferrario@gmail.com>
2024-01-25 08:47:56 +0100

    Use new version of Geant4 in automatic tests

--- docker/geant4/download_dataset.sh
+++ docker/geant4/download_dataset.sh
@@ -4,16 +4,16 @@ DATASETDIR=$1
 DATASETDIR=${DATASETDIR:=data}
 
 DATASETS="
-G4NDL.4.6.tar.gz
-G4EMLOW.8.0.tar.gz
+G4NDL.4.7.tar.gz
+G4EMLOW.8.5.tar.gz
 G4PhotonEvaporation.5.7.tar.gz
 G4RadioactiveDecay.5.6.tar.gz
 G4PARTICLEXS.4.0.tar.gz
 G4PII.1.3.tar.gz
 G4RealSurface.2.2.tar.gz
 G4SAIDDATA.2.0.tar.gz
-G4ABLA.3.1.tar.gz
-G4INCL.1.0.tar.gz
+G4ABLA.3.3.tar.gz
+G4INCL.1.2.tar.gz
 G4ENSDFSTATE.2.3.tar.gz
 "
 
