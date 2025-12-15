650983c9a6d2c5a005a0e4adf020791cd65a8671
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-12-15 16:42:00 +0000

    Kill optical photons when deposited energy out of range

--- source/actions/DefaultStackingAction.cc
+++ source/actions/DefaultStackingAction.cc
@@ -9,10 +9,12 @@
 
 
 #include "DefaultStackingAction.h"
+#include "DefaultEventAction.h"
 #include "FactoryBase.h"
 #include "IonizationElectron.h"
 
 #include <G4OpticalPhoton.hh>
+#include <G4EventManager.hh>
 #include <G4Track.hh>
 
 using namespace nexus;
@@ -47,7 +49,14 @@ DefaultStackingAction::ClassifyNewTrack(const G4Track* track)
 
 void DefaultStackingAction::NewStage()
 {
-  return;
+  /// At this point, tracks in the waiting stack have already been moved
+  /// to the urgent stack, so if the deposited energy does not fall in
+  /// the specified range, we clear them
+  static auto event_action =
+    static_cast<DefaultEventAction*>(G4EventManager::GetEventManager() -> GetUserEventAction());
+
+  if (!event_action -> IsDepositedEnergyInRange())
+    stackManager -> ClearUrgentStack();
 }
 
 
